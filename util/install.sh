7329e0e483e03abb6ad1a1cd754c445befcd573e
Alex Lance <code@alexlance.com>
2006-04-03 16:23:22 +1000

    [alloc @ alla@soy.office.cyber.com.au-20060403062322-7cee6307c2b6df06] miscellaneous util/install cleanup

--- util/install.sh
+++ util/install.sh
@@ -9,6 +9,8 @@ function run {
   echo -e " [${STATUS}]  ${1} ${ERR}"
 }
 
+
+
 find .. -type f -exec chmod 664 {} \;
 find .. -type d -exec chmod 775 {} \;
 find .. -type f -exec chgrp alloc {} \;
@@ -17,14 +19,16 @@ run "chmod 777 ../project/docs"                     # uploaded files
 run "chmod 777 ../client/docs"                      # uploaded files
 run "chmod 777 ../images/"                          # php created images
 run "chmod 777 ../images/big*"                      # php created images
-run "chmod 777 ../images/user*"                      # php created images
+run "chmod 777 ../images/user*"                     # php created images
 run "chmod 777 ../report/files/"                    # uploaded files
-run "chmod 755 ./dump_structure.sh"                 # rwxr-xr-x
-run "chmod 755 ./install.sh"                        # rwxr--r--
-run "chmod 777 ../stylesheets/*"                    #
+run "chmod 755 ./dump_clean_db.sh"                  # rwxr-xr-x
+run "chmod 755 ./alloc_DB_backup.sh"                # rwxr-xr-x
+run "chmod 700 ./install.sh"                        # rwxr-----
+run "chmod 777 ../stylesheets/*"                    # rwxrwxrwx
 run "chmod 754 ./style_regen.py"                    # rwxr-xr-x
 
 run "chown alloc ../logs"                           # gonna be run by user alloc
+[ ! -f "../logs/alloc_email.log" ] && run "touch ../logs/alloc_email.log"
 run "chmod 777 ../logs"                             # gonna need to write and delete
 run "chmod 777 ../logs/alloc_email.log"             # gonna need to write and delete
 run "chmod 755 ../logs/cron_sendReminders.sh"       # rwxr-xr-x 
@@ -38,5 +42,7 @@ run "chmod 755 ../logs/cron_checkRepeatExpenses.sh" # rwxr-xr-x
 
 
 echo "NOW INSTALL THE CRON JOBS";
-cat cron_jobs.txt
+sed -e "s/REPLACEME/${PWD//\//\\/}/" < cron_jobs.txt
+
+
 
