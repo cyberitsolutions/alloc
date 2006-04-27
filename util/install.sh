2c14eb9e11a2300703c30811c876f97a21b9b3ae
Alex Lance <code@alexlance.com>
2006-04-27 16:16:44 +1000

    [alloc @ alla@soy.office.cyber.com.au-20060427061644-2fc2d38c5e578124] Modified install script

--- util/install.sh
+++ util/install.sh
@@ -13,36 +13,36 @@ function run {
 DIR=`dirname ${0}`/
 
 
-find .. -type f -exec chmod 664 {} \;
-find .. -type d -exec chmod 775 {} \;
-find .. -type f -exec chgrp alloc {} \;
+find ${DIR}.. -type f -exec chmod 664 {} \;
+find ${DIR}.. -type d -exec chmod 775 {} \;
+find ${DIR}.. -type f -exec chgrp alloc {} \;
 
-run "chmod 777 ../images/"                          # php created images
-run "chmod 777 ../images/big*"                      # php created images
-run "chmod 777 ../images/user*"                     # php created images
-run "chmod 777 ../report/files/"                    # uploaded files
-run "chmod 777 ../stylesheets/*"                    # rwxrwxrwx
+run "chmod 777 ${DIR}../images/"                          # php created images
+run "chmod 777 ${DIR}../images/big*"                      # php created images
+run "chmod 777 ${DIR}../images/user*"                     # php created images
+run "chmod 777 ${DIR}../report/files/"                    # uploaded files
+run "chmod 777 ${DIR}../stylesheets/*"                    # rwxrwxrwx
 
-run "chmod 755 ./dump_clean_db.sh"                  # rwxr-xr-x
-run "chmod 755 ./alloc_DB_backup.sh"                # rwxr-xr-x
-run "chmod 700 ./install.sh"                        # rwxr-----
-run "chmod 754 ./stylesheet_regen.py"               # rwxr-xr--
-run "chmod 754 ./update_alloc_dev_database.sh"      # rwxr-xr--
-run "chmod 754 ./gpl_header.py"                     # rwxr-xr--
-run "chmod 777 ./INSTALLER_LOCK"                    # rwxrwxrwx
-run "chmod 755 ./cron_sendReminders.sh"             # rwxr-xr-x 
-run "chmod 755 ./cron_sendEmail.sh"                 # rwxr-xr-x
-run "chmod 755 ./cron_checkRepeatExpenses.sh"       # rwxr-xr-x
+run "chmod 755 ${DIR}dump_clean_db.sh"                  # rwxr-xr-x
+run "chmod 755 ${DIR}alloc_DB_backup.sh"                # rwxr-xr-x
+run "chmod 700 ${DIR}install.sh"                        # rwxr-----
+run "chmod 754 ${DIR}stylesheet_regen.py"               # rwxr-xr--
+run "chmod 754 ${DIR}update_alloc_dev_database.sh"      # rwxr-xr--
+run "chmod 754 ${DIR}gpl_header.py"                     # rwxr-xr--
+run "chmod 777 ${DIR}INSTALLER_LOCK"                    # rwxrwxrwx
+run "chmod 755 ${DIR}cron_sendReminders.sh"             # rwxr-xr-x 
+run "chmod 755 ${DIR}cron_sendEmail.sh"                 # rwxr-xr-x
+run "chmod 755 ${DIR}cron_checkRepeatExpenses.sh"       # rwxr-xr-x
 
-run "chown alloc ../logs"                           # gonna be run by user alloc
-[ ! -f "../logs/alloc_email.log" ] && run "touch ../logs/alloc_email.log"
-run "chmod 777 ../logs"                             # gonna need to write and delete
-run "chmod 777 ../logs/alloc_email.log"             # gonna need to write and delete
+run "chown alloc ${DIR}../logs"                           # gonna be run by user alloc
+[ ! -f "${DIR}../logs/alloc_email.log" ] && run "touch ../logs/alloc_email.log"
+run "chmod 777 ${DIR}../logs"                             # gonna need to write and delete
+run "chmod 777 ${DIR}../logs/alloc_email.log"             # gonna need to write and delete
 
-[ -f ../logs/sendReminders_log.new          ] && run "rm -f ../logs/sendReminders_log.new"
-[ -f ../logs/sendReminders_log.txt          ] && run "rm -f ../logs/sendReminders_log.txt"
-[ -f ../logs/sendEmail_log.new              ] && run "rm -f ../logs/sendEmail_log.new"
-[ -f ../logs/checkRepeatingExpenses_log.new ] && run "rm -f ../logs/checkRepeatingExpenses_log.new"
+# [ -f ${DIR}../logs/sendReminders_log.new          ] && run "rm -f ${DIR}../logs/sendReminders_log.new"
+# [ -f ${DIR}../logs/sendReminders_log.txt          ] && run "rm -f ${DIR}../logs/sendReminders_log.txt"
+# [ -f ${DIR}../logs/sendEmail_log.new              ] && run "rm -f ${DIR}../logs/sendEmail_log.new"
+# [ -f ${DIR}../logs/checkRepeatingExpenses_log.new ] && run "rm -f ${DIR}../logs/checkRepeatingExpenses_log.new"
 
 
 #sed -e "s/REPLACEME/${PWD//\//\\/}/" < cronjobs.txt
@@ -52,10 +52,10 @@ run "chmod 777 ../logs/alloc_email.log"             # gonna need to write and de
 
 echo "Now install the cron jobs:";
 cat <<EOF
-*/5 * * * * ${PWD}/cron_sendReminders.sh
-0   4 * * * ${PWD}/alloc_DB_backup.sh
-5   4 * * * ${PWD}/cron_sendEmail.sh
-10  4 * * * ${PWD}/cron_checkRepeatExpenses.sh
+*/5 * * * * /path/to/alloc/util/cron_sendReminders.sh
+0   4 * * * /path/to/alloc/util/alloc_DB_backup.sh
+5   4 * * * /path/to/alloc/util/cron_sendEmail.sh
+10  4 * * * /path/to/alloc/util/cron_checkRepeatExpenses.sh
 EOF
 
 
