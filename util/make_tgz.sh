387943d42974a6618ec2e7d2b785020a18185af0
Alex Lance <code@alexlance.com>
2006-04-29 15:06:41 +1000

    [alloc @ alla@soy.office.cyber.com.au-20060429050641-f51e6c89bc30729d] Tweak to make tgz script

--- util/make_tgz.sh
+++ util/make_tgz.sh
@@ -2,11 +2,11 @@
 
 
 
-cd /cyber/devel/bzr/alloc
-
-bzr push ~/allocPSA
+cd /cyber/devel/bzr/alloc/ && bzr push ~/allocPSA
 cd ~
 tar czvf allocPSA.tgz allocPSA
+\rm -rf allocPSA
 mv allocPSA.tgz html/
+
 
 
