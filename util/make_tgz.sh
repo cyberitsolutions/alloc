0a7af028272e1c1805068673162dc9867b0d3021
Alex Lance <code@alexlance.com>
2006-05-06 21:31:36 +1000

    [alloc @ alla@soy.office.cyber.com.au-20060506113136-61a53f87b53c907e] Added version number to make tgz script

--- util/make_tgz.sh
+++ util/make_tgz.sh
@@ -4,7 +4,7 @@ revno=$(bzr revno)
 bzr push
 cd /cyber/devel/bzr/alloc/ && bzr export --format tgz ~/allocPSA.${revno}.tgz 
 cd ~
-mv allocPSA.${revno}.tgz html/
+mv allocPSA-1.2.${revno}.tgz html/
 
 
 
