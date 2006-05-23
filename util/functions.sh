1c5c6fccdfa2e7dee221196ec44283238b6c22a6
Alex Lance <code@alexlance.com>
2006-05-24 03:32:50 +1000

    [alloc @ alla@mint-20060523173250-3efe3fa7ab2a6e9e] Made make_executables able to be run standalone. Moved CONFIG_VARS list into functions.sh. Added make_clean.sh script

--- util/functions.sh
+++ util/functions.sh
@@ -1,6 +1,30 @@
-# Functions to help display messages 
+
+#  Copyright 2006, Alex Lance, Clancy Malcolm, Cybersource Pty. Ltd.
+#  
+#  This file is part of allocPSA <info@cyber.com.au>.
+#  
+#  allocPSA is free software; you can redistribute it and/or modify it under the
+#  terms of the GNU General Public License as published by the Free Software
+#  Foundation; either version 2 of the License, or (at your option) any later
+#  version.
+#  
+#  allocPSA is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+#  A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License along with
+#  allocPSA; if not, write to the Free Software Foundation, Inc., 51 Franklin
+#  St, Fifth Floor, Boston, MA 02110-1301 USA
 # 
 
+
+# A list of all the variable set in this file, as a form of checking the install.cfg file
+DIR_VARS="ALLOC_DOCS_DIR ALLOC_BACKUP_DIR ALLOC_LOG_DIR ALLOC_PATCH_DIR ALLOC_WEB_URL_PREFIX"
+CONFIG_VARS="ALLOC_DB_NAME ALLOC_DB_USER ALLOC_DB_PASS ALLOC_DB_HOST ALLOC_WEB_USER ${DIR_VARS}"
+
+
+# Functions to help display messages 
+# 
 function e_red      { echo -en "\x1b[31;01m${1}\x1b[0m"; }
 function e_green    { echo -en "\x1b[32;01m${1}\x1b[0m"; }
 function e_yellow   { echo -en "\x1b[33;01m${1}\x1b[0m"; }
@@ -16,7 +40,6 @@ function e_n        { e_blue "\n* "; e_white_b "${1}"; }
 
 function e_good     { e_green "\n* * * "; e_white_b "${1}"; e_green " * * *\n"; }
 function e_bad      { e_red "\n* * * ";   e_white_b "${1}"; e_red " * * *\n"; }
-
 
 
 # Execute command, catch errors.
@@ -98,6 +121,25 @@ function die {
   e_dead "Exiting: ${1}"
   exit
 }
+
+# Append slashes to the end of the dirs
+#
+function make_config_dirs_end_in_slashes {
+  #for i in ${DIR_VARS}; do echo "  ${i}: ${!i}"; done
+  for i in ${DIR_VARS}; do 
+    [ "${!i:(-1):1}" != "/" ] && eval "${i}"="${!i}/"
+  done
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
