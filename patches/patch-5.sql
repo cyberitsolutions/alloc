e263ba84b070d0b207ebb40717a779eff4f860fb
Alex Lance <code@alexlance.com>
2006-05-29 03:15:41 +1000

    [alloc @ alla@soy.office.cyber.com.au-20060528171541-db3da4164b55ab4d] Added test patches for testing the patch.sh functionality in livealloc

---
+++ patches/patch-5.sql
@@ -0,0 +1,4 @@
+
+-- Insert a lowlevel user
+INSERT INTO person (username, password, perms, personActive) VALUES ('tester','/.iTV2iP8pLgs','employee',1);
+
---
+++ patches/patch-6.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "hey patch-6.sh is running!!"
+
