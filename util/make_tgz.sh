4d5d25a1ecfe3299e34ee40bbb6e957dc9dcb9b7
Alex Lance <code@alexlance.com>
2006-05-29 20:55:17 +1000

    [alloc @ alla@peach-20060529105517-66aebe1bbd69cd3f] Made make_tgz merge in the html help file.

--- util/make_tgz.sh
+++ util/make_tgz.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
 revno=$(bzr revno)
-file="/home/${USER}/html/allocPSA-1.2.${revno}.tgz"
+
+dir="/home/${USER}/html"
+f1="allocPSA-1.2.${revno}"
+f2="allocPSA_doc-1.2.${revno}"
+dir1="${dir}/${f1}"
+dir2="${dir}/${f2}"
+
+[ -d "${dir1}" ] && \rm -rf ${dir1}
 
 bzr push
-cd /cyber/devel/bzr/alloc/ && bzr export --format tgz ${file}
+cd /cyber/devel/bzr/alloc/ && bzr export --format dir ${dir1}
+cd /cyber/devel/bzr/alloc_doc/ && bzr export --format dir ${dir2}
 
-if [ -f "${file}" ]; then
-  echo "Created: ${file}"
+cd ${dir2}
+
+PATH=/usr/bin/:${PATH}
+
+make help.html
+mv help.html ${dir1}/help/
+mv help.css ${dir1}/help/
+mv images ${dir1}/help/
+
+\rm -rf ${dir2}
+
+cd ${dir}
+tar czvf ${f1}.tgz ${f1}
+
+
+if [ -f "${f1}.tgz" ]; then
+  echo "Created: ${f1}.tgz"
+  rm -rf ${f1}
 else 
-  echo "Problem creating: ${file}"
+  echo "Problem creating: ${f1}.tgz"
 fi
+
 
