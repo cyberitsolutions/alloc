bba2be900faef5c7c228bd12550d3cd260c676f4
Alex Lance <alla@peach>
2006-06-01 22:29:35 +1000

    [alloc @ alla@peach-20060601122935-3a92438290a428c4] Modded make_tgz.sh to use make_doc.sh

--- util/make_tgz.sh
+++ util/make_tgz.sh
@@ -7,24 +7,18 @@ revno=$(bzr revno)
 
 d="/home/${USER}/html"
 f1="allocPSA-1.2.${revno}"
-f2="allocPSA_doc-1.2.${revno}"
 dir1="${d}/${f1}"
-dir2="${d}/${f2}"
 
 [ -d "${dir1}" ] && \rm -rf ${dir1}
 
 bzr push
 cd /cyber/devel/bzr/alloc/ && bzr export --format dir ${dir1}
-cd /cyber/devel/bzr/alloc_doc/ && bzr export --format dir ${dir2}
 
-cd ${dir2}
 
 PATH=/usr/bin/:${PATH}
 
-make help.html
-mv help.html ${dir1}/help/
-mv help.css ${dir1}/help/
-mv images ${dir1}/help/
+cd ${dir1}/util/
+./make_doc.sh
 
 \rm -rf ${dir2}
 
