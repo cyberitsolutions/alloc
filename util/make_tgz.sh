469e29a1423be723ae45e2dcbc5ab6129cc24569
Alex Lance <alla@peach>
2006-09-27 00:10:50 +1000

    [alloc @ alla@peach-20060926141050-70bcce411b22dfb8] Added a patch for the make_tgz script to utilise the alloc_version script instead of having the version hardcoded.

--- util/make_tgz.sh
+++ util/make_tgz.sh
@@ -3,10 +3,11 @@
 
 DIR="${0%/*}/"
 
-revno=$(bzr revno)
-
 d="/home/${USER}/html"
-f1="allocPSA-1.2.${revno}"
+
+version="$(cat ${DIR}alloc_version)"
+f1="allocPSA-${version}"
+
 dir1="${d}/${f1}"
 
 [ -d "${dir1}" ] && \rm -rf ${dir1}
@@ -28,7 +29,7 @@ cd ${d}
 tar czvf ${f1}.tgz ${f1}
 
 if [ -f "${f1}.tgz" ]; then
-  echo "Created: ${f1}.tgz"
+  echo "Created: ${d}/${f1}.tgz"
   rm -rf ${f1}
 else 
   echo "Problem creating: ${f1}.tgz"
