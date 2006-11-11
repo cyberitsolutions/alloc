58a5c9ba2a6fc6cd2053c3a7f0d1bc90534c3f33
alla <alla@mint>
2006-11-11 16:16:38 +1100

    [alloc @ alla@mint-20061111051638-11bb949b08c79453] Added some new features to the template engine.

--- shared/lib/template.inc.php
+++ shared/lib/template.inc.php
@@ -31,34 +31,57 @@ function get_template($filename, $use_function_object = false) {
     echo "get_template() failure: [$filename]";
   }
 
-  // Replace {var_name} with echo $TPL["var_name"]
-  $pattern = '/{([\w|\d|_]+)}/i';
-  $replace = '<?php echo stripslashes($TPL["${1}"]); ?>';
-  $template = preg_replace($pattern,$replace,$template);
-
-  // Replace {:function_name param} with function("param");
+  // Replace {:function_name param} with function("param"); - REMOVE THIS SOMETIME!
   if ($use_function_object) {
-    $pattern = '/{:(\w+)\s?([^}]*)}/i';
-    $replace = '<?php $function_object->${1}("${2}"); ?>';
-    $template = preg_replace($pattern,$replace,$template);
-
-  } else {
-    $pattern = '/{:(\w+)\s?([^}]*)}/i';
-    $replace = '<?php ${1}("${2}"); ?>';
+    $pattern = '/{(\w+)\s?([^}]*)}/i';
+    $replace = '<?php $function_object->${1}${2}; ?>';
     $template = preg_replace($pattern,$replace,$template);
   }
 
-  $pattern = '/{optional:([\w]+) ?}/i';
-  $replace = '<?php if (check_optional_${1}()) { ?>';
+  // Replace {$var_name} with echo stripslashes($TPL["var_name"]); 
+  $pattern = '/{\$([\w|\d|_]+)}/i';
+  $replace = '<?php echo stripslashes($TPL["${1}"]); ?>';
   $template = preg_replace($pattern,$replace,$template);
 
-  $pattern = '/{\/optional}/i';
-  $replace = '<?php } ?>';
+  // Replace {if hey}    with if (hey) { 
+  // Replace {while hey} with while (hey) { 
+  $pattern = '/{(if|while)+ ([^}]*)}/i';
+  $replace = '<?php ${1} (${2}) TPL_START_BRACE ?>';
   $template = preg_replace($pattern,$replace,$template);
 
-  $pattern = '.php&';
-  $replace = '.php?';
+  
+  // Replace {else with {TPL_END_BRACE else
+  $pattern = '{else';
+  $replace = '{TPL_END_BRACE else';
   $template = str_replace($pattern,$replace,$template);
+
+  // Replace {TPL_END_BRACE else} with {TPL_END_BRACE else TPL_START_BRACE
+  $pattern = '{TPL_END_BRACE else}';
+  $replace = '{TPL_END_BRACE else TPL_START_BRACE}';
+  $template = str_replace($pattern,$replace,$template);
+
+  // Replace {TPL_END_BRACE else if hey} with {TPL_END_BRACE else if (hey) TPL_START_BRACE}
+  $pattern = '/{TPL_END_BRACE else\s?if ([^}]+)}/i';
+  $replace = '{TPL_END_BRACE else if (${1}) TPL_START_BRACE}';
+  $template = preg_replace($pattern,$replace,$template);
+
+  
+  $sr = array("{/}"             => "<?php TPL_END_BRACE ?>"
+             ,"{\n"             => "TPL_START_BRACE\n"      // Javascript brace workaround
+             ,"\n}"             => "\nTPL_END_BRACE\n"      // Javascript brace workaround
+             ,"{"               => "<?php "
+             ,"}"               => " ?>"
+             ,"TPL_END_BRACE"   => "}"
+             ,"TPL_START_BRACE" => "{"
+             ,".php&"           => ".php?"
+            );
+
+  foreach ($sr as $s => $r) {
+    $searches[] = $s;
+    $replaces[] = $r;
+  }
+  $template = str_replace($searches,$replaces,$template);
+
 
   return "?>$template<?php ";
 }
@@ -67,11 +90,11 @@ function get_template($filename, $use_function_object = false) {
 // This is the publically callable function, used to include template files
 function include_template($filename, $function_object = "") {
   global $TPL;
-  echo "\n<!-- start $filename -->\n";
+  echo "\n<!-- Start $filename -->\n";
   $template = get_template($filename, is_object($function_object));
-  #echo htmlspecialchars($template); die();
+  #echo "<pre>".htmlspecialchars($template)."</pre>"; 
   eval($template);
-  echo "\n<!-- end $filename -->\n";
+  echo "\n<!-- End $filename -->\n";
 } 
 
 
