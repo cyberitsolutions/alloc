c823555242581a10852289ed51fe1e33119247e6
Alex Lance <code@alexlance.com>
2006-11-17 17:16:54 +1100

    [alloc @ alla@mint-20061117061654-1de324494c6bdb51] Fixed bug where couldn't create new TF because tfBalance NOT NULL was inserting as NULL.

--- finance/tf.php
+++ finance/tf.php
@@ -93,6 +93,7 @@ if ($_POST["save"]) {
     if ($tf_is_taken) {
       $TPL["message"][] = "That TF name is taken, please choose another.";
     } else {
+      $tf->set_value("tfBalance","0.0");
       $tf->save();
       $TPL["message_good"][] = "Your TF has been saved.";
     }
