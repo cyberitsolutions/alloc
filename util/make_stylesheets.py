e643102e0cc7875bbcfddcda7abbfda0cf766dcc
Alex Lance <code@alexlance.com>
2007-06-11 06:10:01 +1000

    [alloc @ alla@cyber.com.au-20070610201001-kc34o3ruf3zop84b] Moved style.tpl into styles/

--- util/make_stylesheets.py
+++ util/make_stylesheets.py
@@ -11,7 +11,7 @@ d = os.path.dirname(sys.argv[0])+'/'
 
 
 # Convert the style template into a string
-fd = open(d+'../style.tpl')
+fd = open(d+'../styles/style.tpl')
 str_orig = fd.read();
 fd.close();
 
@@ -22,57 +22,58 @@ files = os.listdir(d+'../styles/')
 # Loop through files
 for item in files:
 
+  if item != 'style.tpl':
 
-  # Parse each style_something.ini file
-  config.read([d+'../styles/'+item])
+    # Parse each style_something.ini file
+    config.read([d+'../styles/'+item])
 
-  # Get a list of sections from the config file
-  sections = config.sections()
-
-
-  # Loop through each section
-  for section in sections:
-    filename_suffix = 0;
-    dict = {}
-    str = str_orig
+    # Get a list of sections from the config file
+    sections = config.sections()
 
 
-    # Get all "keys:" from the ini file
-    options = config.options(section)
-
-    # Load up a dict 
-    for option in options:
-        dict[option] = config.get(section,option)
-    
-    # Do a search and replace for each key/value in the template string 
-    for k, v in dict.items():
-      str = str.replace('('+k.upper()+')', v)
+    # Loop through each section
+    for section in sections:
+      filename_suffix = 0;
+      dict = {}
+      str = str_orig
 
 
-    # Create matrix of stylesheets, themes vs font sizes 
-    for weight in range(-3,7):
-      filename_suffix += 1
-      str2 = str;
+      # Get all "keys:" from the ini file
+      options = config.options(section)
 
-      font_sizes = {}
-      font_sizes['H1_FONT_SIZE']                = (16 + weight)
-      font_sizes['H2_FONT_SIZE']                = (16 + weight)
-      font_sizes['H3_FONT_SIZE']                = (15 + weight)
-      font_sizes['TABLE_BOX_TH_FONT_SIZE']      = (13 + weight)
-      font_sizes['TABLE_BOX_TH_A_FONT_SIZE']    = (13 + weight)
-      font_sizes['TABLE_TOOLBAR_TH_FONT_SIZE']  = (17 + weight)
-      font_sizes['TABLE_TOOLBAR_TD_FONT_SIZE']  = (13 + weight)
-      font_sizes['DEFAULT_FONT_SIZE']           = (12 + weight)   
+      # Load up a dict 
+      for option in options:
+          dict[option] = config.get(section,option)
+      
+      # Do a search and replace for each key/value in the template string 
+      for k, v in dict.items():
+        str = str.replace('('+k.upper()+')', v)
 
-    
-      for k, v in font_sizes.items():
-        str2 = str2.replace('('+k.upper()+')', repr(v))
+
+      # Create matrix of stylesheets, themes vs font sizes 
+      for weight in range(-3,7):
+        filename_suffix += 1
+        str2 = str;
+
+        font_sizes = {}
+        font_sizes['H1_FONT_SIZE']                = (16 + weight)
+        font_sizes['H2_FONT_SIZE']                = (16 + weight)
+        font_sizes['H3_FONT_SIZE']                = (15 + weight)
+        font_sizes['TABLE_BOX_TH_FONT_SIZE']      = (13 + weight)
+        font_sizes['TABLE_BOX_TH_A_FONT_SIZE']    = (13 + weight)
+        font_sizes['TABLE_TOOLBAR_TH_FONT_SIZE']  = (17 + weight)
+        font_sizes['TABLE_TOOLBAR_TD_FONT_SIZE']  = (13 + weight)
+        font_sizes['DEFAULT_FONT_SIZE']           = (12 + weight)   
 
       
-      # Write it out to a file
-      fd = open(d+'../css/'+section+'_'+repr(filename_suffix)+'.css','w')
-      fd.write(str2) 
-      fd.close()
+        for k, v in font_sizes.items():
+          str2 = str2.replace('('+k.upper()+')', repr(v))
+
+        
+        # Write it out to a file
+        fd = open(d+'../css/'+section+'_'+repr(filename_suffix)+'.css','w')
+        fd.write(str2) 
+        fd.close()
 
 
 
