441e31e39d6e20ce1f73d28e3cb1fda7dcdba236
Alex Lance <code@alexlance.com>
2007-12-18 15:06:54 +1100

    11447 Updated patch file to not update the invoice.clientID with a best guess.

--- patches/patch-00065-alla.php
+++ patches/patch-00065-alla.php
@@ -1,35 +1,21 @@
 <?php
-// UPDATE the invoice.clientID field, by attempting to determine which
-// client each invoice belongs to via the invoice.invoiceName field.
+// Remove the commenting and run the patch to attempt to update the
+// invoice.clientID field with a best guess based on the invoice.invoiceName
+// field.
 
-#$hardcoded['Finsys Pty Ltd'] = 
-#$hardcoded['DOJ'] = 
-#$hardcoded['OSV'] = 1823;
-#$hardcoded['Marker Solutions and Services'] = 3363;
-#$hardcoded[''] = ;
-#$hardcoded[''] = ;
-#$hardcoded[''] = ;
-#$hardcoded[''] = ;
-#$hardcoded[''] = ;
-#$hardcoded[''] = ;
-#$hardcoded[''] = ;
-#$hardcoded[''] = ;
-#$hardcoded[''] = ;
-
-
-$db = new db_alloc();
-$db2 = new db_alloc();
-
-$q = sprintf("SELECT distinct invoiceName FROM invoice");
-$db->query($q);
-
-while ($db->next_record()) {
-
-  list($probable_clientID,$client_percent) = get_clientID_from_name($db->f("invoiceName"));
-
-  if ($client_percent > 80) {
-    $q = sprintf("UPDATE invoice SET clientID = %d WHERE invoiceName = '%s'",$probable_clientID,db_esc($db->f("invoiceName")));
-    $db2->query($q);
-  }
-}
+#$db = new db_alloc();
+#$db2 = new db_alloc();
+#
+#$q = sprintf("SELECT distinct invoiceName FROM invoice");
+#$db->query($q);
+#
+#while ($db->next_record()) {
+#
+#  list($probable_clientID,$client_percent) = get_clientID_from_name($db->f("invoiceName"));
+#
+#  if ($client_percent > 80) {
+#    $q = sprintf("UPDATE invoice SET clientID = %d WHERE invoiceName = '%s'",$probable_clientID,db_esc($db->f("invoiceName")));
+#    $db2->query($q);
+#  }
+#}
 ?>
