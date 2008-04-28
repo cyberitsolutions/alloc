584eac7d0bee57121842ce8c05e2aa438dbac76a
Alex Lance <alla@cyber.com.au>
2008-04-28 18:13:26 +1000

    12524 Made whatsnew work when called from any directory.

--- whatsnew.sh
+++ whatsnew.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-BACKUP_DIR="$(cat ./alloc_config.php  | grep ATTACHMENTS_DIR  | sed -e 's/define("ATTACHMENTS_DIR","//' | sed -e 's/");$//')";
+cwd="${0%/*}/"
+cd $cwd
+
+BACKUP_DIR="$(cat alloc_config.php  | grep ATTACHMENTS_DIR  | sed -e 's/define("ATTACHMENTS_DIR","//' | sed -e 's/");$//')";
 
 if [ -d "${BACKUP_DIR}" ]; then
   file="${BACKUP_DIR}whatsnew/0/$(date +%Y-%m-%d)"
