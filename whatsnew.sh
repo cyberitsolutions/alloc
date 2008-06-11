0d7ee6faf9d9d95115be3f4e979a3d1a481e2b64
Alex Lance <code@alexlance.com>
2008-06-11 13:35:26 +1000

    20020 Modify whatsnew mechanism to use DARCS_PATCHES darcs environment variable

--- whatsnew.sh
+++ whatsnew.sh
@@ -7,8 +7,7 @@ BACKUP_DIR="$(cat alloc_config.php  | grep ATTACHMENTS_DIR  | sed -e 's/define("
 
 if [ -d "${BACKUP_DIR}" ]; then
   file="${BACKUP_DIR}whatsnew/0/$(date +%Y-%m-%d)"
-  #echo -e "\n" >> $file
-  info=$(darcs changes --last ${1} | sed -e 's/^[^\*]*$//' | sed -e 's/ \* /<li>/')
+  info=$(echo "${1}" | sed -e 's/^[^\*]*$//' | sed -e 's/ \* /<li>/')
   echo $info >> $file
 fi
 
