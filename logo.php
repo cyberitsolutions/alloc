58bcf172afc1c5175da08f3f2b298acc363c1c5c
Dinis Lage <dinislage@senolage.com>
2010-04-30 00:27:54 +1000

    Html Logo TimeSheet Logo included in the Html TimeSheet report. /logo.php now allows for different sizes of the logo. (logo.php?size=big for the original, no other options yet).

--- logo.php
+++ logo.php
@@ -1,5 +1,9 @@
 <?php
 include('alloc_config.php');
 header('Content-type: image/png');
-echo file_get_contents(ATTACHMENTS_DIR.'/logos/logo_small.png');
+if(isset($_GET['size']) && $_GET['size'] == 'big') {
+  echo file_get_contents(ATTACHMENTS_DIR.'/logos/logo.png');
+} else {
+  echo file_get_contents(ATTACHMENTS_DIR.'/logos/logo_small.png');
+}
 ?>
