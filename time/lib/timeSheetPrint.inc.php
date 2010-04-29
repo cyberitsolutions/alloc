9fc07ce27705543e2e9db94b3e7418908ba951d6
Dinis Lage <dinislage@gmail.com>
2010-04-30 00:27:54 +1000

    Html Logo TimeSheet Logo included in the Html TimeSheet report. /logo.php now allows for different sizes of the logo. (logo.php?size=big for the original, no other options yet).

--- time/lib/timeSheetPrint.inc.php
+++ time/lib/timeSheetPrint.inc.php
@@ -441,6 +441,9 @@ class timeSheetPrint {
 
       // Else HTML format
       } else {
+        if(file_exists(ATTACHMENTS_DIR.'logos/logo.png')) {
+          $TPL["companyName"] = '<img src="/logo.php?size=big" alt="'.$TPL["companyName"].'" height="70px" />';
+        }
         $TPL["this_tsp"] = $this;
         $TPL["main_alloc_title"] = "Time Sheet - ".APPLICATION_NAME;
         include_template(dirname(__FILE__)."/../templates/timeSheetPrintM.tpl");
