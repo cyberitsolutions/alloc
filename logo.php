76b38156ae548c88a3650ee23bcbd9ca22870036
Dinis Lage <dinislage@senolage.com>
2010-04-30 00:46:10 +1000

    logo.php for all images currently used.

--- logo.php
+++ logo.php
@@ -1,9 +1,16 @@
 <?php
 include('alloc_config.php');
-header('Content-type: image/png');
-if(isset($_GET['size']) && $_GET['size'] == 'big') {
-  echo file_get_contents(ATTACHMENTS_DIR.'/logos/logo.png');
-} else {
-  echo file_get_contents(ATTACHMENTS_DIR.'/logos/logo_small.png');
+$path = ATTACHMENTS_DIR.'logos/';
+$image = 'logo_small.png';
+$type = 'png';
+if(isset($_GET['size'])) {
+  if($_GET['size'] == 'big') {
+    $image = 'logo.png';
+  } else if($_GET['size'] == 'jpg') {
+    $type = 'jpeg';
+    $image = 'logo.jpg';
+  }
 }
+header('Content-type: image/'.$type);
+echo file_get_contents($path.$image);
 ?>
