3f66439a8348581680d846d6e9cd782b4a174cd7
Alex Lance <alla@cyber.com.au>
2010-08-16 20:40:45 +1000

    Added a span tag around comment dates to aid in bookmarklet scripting.

--- comment/lib/comment.inc.php
+++ comment/lib/comment.inc.php
@@ -244,7 +244,7 @@ class comment extends db_entity {
   }
 
   function get_comment_attribution($comment=array()) {
-    $str = '<b>'.comment::get_comment_author($comment).'</b> '.$comment["date"];
+    $str = '<b>'.comment::get_comment_author($comment).'</b> <span class="comment_date">'.$comment["date"]."</span>";
       if ($comment["commentModifiedTime"] || $comment["commentModifiedUser"]) {
         $str.= ", last modified by <b>".person::get_fullname($comment["commentModifiedUser"])."</b> ".$comment["commentModifiedTime"];
       }
