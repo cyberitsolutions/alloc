04e1f36df93620d35a8794fd7081f81a86883a75
Alex Lance <alla@cyber.com.au>
2010-11-27 14:51:48 +1100

    17316 Updated patch to allow other instances of alloc to skip the big comment/email fix.

--- patches/patch-00179-alla.php
+++ patches/patch-00179-alla.php
@@ -74,7 +74,6 @@ function fix_this_comment($r,$num,$from,$messageid) {
 
 
 global $fp;
-$fp = fopen(ATTACHMENTS_DIR."updatecommentpatch179.log","a+");
 
 
 // nuke output buffering
@@ -103,129 +102,131 @@ $info["password"] = config::get_config_item("allocEmailPassword");
 $info["protocol"] = config::get_config_item("allocEmailProtocol");
 
 if (!$info["host"]) {
-  die("Email mailbox host not defined, assuming email receive function is inactive.");
-}
+  //die("Email mailbox host not defined, assuming email receive function is inactive.");
+  define("FORCE_PATCH_SUCCEED_patch-00179-alla.php",1);
+} else {
 
-$mail = new alloc_email_receive($info,$lockfile);
-$mail->open_mailbox(config::get_config_item("allocEmailFolder"),OP_HALFOPEN+OP_READONLY);
-$mail->check_mail();
+  $fp = fopen(ATTACHMENTS_DIR."updatecommentpatch179.log","a+");
+  $mail = new alloc_email_receive($info,$lockfile);
+  $mail->open_mailbox(config::get_config_item("allocEmailFolder"),OP_HALFOPEN+OP_READONLY);
+  $mail->check_mail();
 
-$msg_nums = $mail->get_all_email_msg_uids(); 
+  $msg_nums = $mail->get_all_email_msg_uids(); 
 
-# foreach ($msg_nums as $n) {
-#   $n == 326 and $go = true;
-#   $go and $remaining[] = $n;
-# }
-# unset($msg_nums);
-# $msg_nums = $remaining;
+  # foreach ($msg_nums as $n) {
+  #   $n == 326 and $go = true;
+  #   $go and $remaining[] = $n;
+  # }
+  # unset($msg_nums);
+  # $msg_nums = $remaining;
 
-printorlog("\n");
-printorlog(date("Y-m-d H:i:s")." Found ".count($msg_nums)." emails.");
-
-$db = new db_alloc();
-
-// fetch and parse email
-foreach ($msg_nums as $num) {
-
-  // this will stream output
-  flush();
-
-  $dir = ATTACHMENTS_DIR."tmp";
-  if (!is_dir($dir)) mkdir($dir);
-
-  usleep(25000);
-
-  $x++; 
-  #$x>20 and die(fclose($fp).$mail->close()."\n"."Stopped."."\n");
-
-  $mail->set_msg($num);
-  $decoded = $mail->save_email($dir.DIRECTORY_SEPARATOR."email");
-  $body = trim(mime_parser::get_body_text($decoded));
-  $from = $decoded[0]["Headers"]["from:"];
-  $subject = $decoded[0]["Headers"]["subject:"];
-  $messageid = $decoded[0]["Headers"]["message-id:"];
-  $keys = $mail->get_hashes();
-  
   printorlog("\n");
-  printorlog($x.". ".date("Y-m-d H:i:s")." Keys:".print_r($keys,1)." IMAP UID: ".$num);
+  printorlog(date("Y-m-d H:i:s")." Found ".count($msg_nums)." emails.");
 
-  // Get a bunch of comments that look like they might belong to this email...
+  $db = new db_alloc();
 
-  unset($q);
+  // fetch and parse email
+  foreach ($msg_nums as $num) {
 
-  // First try and get a Key: from the subject line, or message-id
-  $key = current($keys);
-  if ($key && strlen($key) == 8) {
-    $db->query("SELECT * FROM token WHERE tokenHash = '".db_esc($key)."'");
-    $row = $db->row();
-    printorlog("Using key: ".$key." relates to: ".$row["tokenEntity"].":".$row["tokenEntityID"]);
-    // lookup the entity for that key eg: comment 1234 or task 123
-    if ($row) {
-      $q = sprintf("SELECT * 
+    // this will stream output
+    flush();
+
+    $dir = ATTACHMENTS_DIR."tmp";
+    if (!is_dir($dir)) mkdir($dir);
+
+    usleep(25000);
+
+    $x++; 
+    #$x>20 and die(fclose($fp).$mail->close()."\n"."Stopped."."\n");
+
+    $mail->set_msg($num);
+    $decoded = $mail->save_email($dir.DIRECTORY_SEPARATOR."email");
+    $body = trim(mime_parser::get_body_text($decoded));
+    $from = $decoded[0]["Headers"]["from:"];
+    $subject = $decoded[0]["Headers"]["subject:"];
+    $messageid = $decoded[0]["Headers"]["message-id:"];
+    $keys = $mail->get_hashes();
+    
+    printorlog("\n");
+    printorlog($x.". ".date("Y-m-d H:i:s")." Keys:".print_r($keys,1)." IMAP UID: ".$num);
+
+    // Get a bunch of comments that look like they might belong to this email...
+
+    unset($q);
+
+    // First try and get a Key: from the subject line, or message-id
+    $key = current($keys);
+    if ($key && strlen($key) == 8) {
+      $db->query("SELECT * FROM token WHERE tokenHash = '".db_esc($key)."'");
+      $row = $db->row();
+      printorlog("Using key: ".$key." relates to: ".$row["tokenEntity"].":".$row["tokenEntityID"]);
+      // lookup the entity for that key eg: comment 1234 or task 123
+      if ($row) {
+        $q = sprintf("SELECT * 
+                        FROM comment 
+                       WHERE commentEmailUID IS NULL 
+                         AND ((commentID = %d AND '%s'='comment') OR (commentLinkID = %d AND commentType='%s'))"
+                    ,$row["tokenEntity"],$row["tokenEntityID"],$row["tokenEntityID"],$row["tokenEntity"]);
+      }
+    
+    // Failing a key, try and get a "Task Comment: 213" from the email subject
+    } else if (preg_match("/Task Comment: (\d+)/", $subject, $matches)) {
+      printorlog("Using subject line: ".$matches[1]." relates to task:".$matches[1]);
+      $q = sprintf("SELECT *
                       FROM comment 
                      WHERE commentEmailUID IS NULL 
-                       AND ((commentID = %d AND '%s'='comment') OR (commentLinkID = %d AND commentType='%s'))"
-                  ,$row["tokenEntity"],$row["tokenEntityID"],$row["tokenEntityID"],$row["tokenEntity"]);
-    }
-  
-  // Failing a key, try and get a "Task Comment: 213" from the email subject
-  } else if (preg_match("/Task Comment: (\d+)/", $subject, $matches)) {
-    printorlog("Using subject line: ".$matches[1]." relates to task:".$matches[1]);
-    $q = sprintf("SELECT *
-                    FROM comment 
-                   WHERE commentEmailUID IS NULL 
-                     AND commentMaster = 'task' 
-                     AND commentMasterID = '%d'",db_esc($matches[1]));
-  } else {
-    printorlog("WTF:".$num." ".$from.": ".print_r($mail->mail_headers,1).$body);
-  }
-
-
-  if ($q) {
-    $db->query($q);
-    $hits = array();
-    while($row = $db->row()) {
-      if (trim($body) == trim($row["comment"])) {
-        $hits[] = $row;
-      }
-    }
-
-    if (count($hits) == 1) {
-      fix_this_comment(current($hits),$num,$from,$messageid);
-
+                       AND commentMaster = 'task' 
+                       AND commentMasterID = '%d'",db_esc($matches[1]));
     } else {
-      printorlog("Found ".count($hits),"orange");
-      // perform more checks to determine which email relates to which comment ...
-      unset($this_is_the_girl);
-      foreach ($hits as $hit) {
-        $from == $hit["commentCreatedUserText"] and $this_is_the_girl = $hit;
-      }
+      printorlog("WTF:".$num." ".$from.": ".print_r($mail->mail_headers,1).$body);
+    }
 
-      if ($this_is_the_girl) {
-        fix_this_comment($this_is_the_girl,$num,$from,$messageid);
-      } else {
-        list($from_address,$from_name) = parse_email_address($from);
-        if (!in_array($from_address,$alloc_from_addresses2)) { // don't update items that are from alloc
-          printorlog("Unable to find comment for email with UID: ".$num." From: ".$from." Subject: ".$subject,"red");
-        } else {
-          printorlog("Skipping UID: ".$num." because it looks to be from alloc: ".$from, "#cccccc");
+
+    if ($q) {
+      $db->query($q);
+      $hits = array();
+      while($row = $db->row()) {
+        if (trim($body) == trim($row["comment"])) {
+          $hits[] = $row;
         }
       }
+
+      if (count($hits) == 1) {
+        fix_this_comment(current($hits),$num,$from,$messageid);
+
+      } else {
+        printorlog("Found ".count($hits),"orange");
+        // perform more checks to determine which email relates to which comment ...
+        unset($this_is_the_girl);
+        foreach ($hits as $hit) {
+          $from == $hit["commentCreatedUserText"] and $this_is_the_girl = $hit;
+        }
+
+        if ($this_is_the_girl) {
+          fix_this_comment($this_is_the_girl,$num,$from,$messageid);
+        } else {
+          list($from_address,$from_name) = parse_email_address($from);
+          if (!in_array($from_address,$alloc_from_addresses2)) { // don't update items that are from alloc
+            printorlog("Unable to find comment for email with UID: ".$num." From: ".$from." Subject: ".$subject,"red");
+          } else {
+            printorlog("Skipping UID: ".$num." because it looks to be from alloc: ".$from, "#cccccc");
+          }
+        }
+      }
+      
     }
     
   }
-  
+
+  printorlog("\n");
+  printorlog(date("Y-m-d H:i:s")." DONE.");
+
+  fclose($fp);
+  $mail->close();
+
+  // This patch can have output, and still proceed as though it has applied cleanly.
+  define("FORCE_PATCH_SUCCEED_patch-00179-alla.php",1);
 }
-
-printorlog("\n");
-printorlog(date("Y-m-d H:i:s")." DONE.");
-
-fclose($fp);
-$mail->close();
-
-// This patch can have output, and still proceed as though it has applied cleanly.
-define("FORCE_PATCH_SUCCEED_patch-00179-alla.php",1);
-
 
 
 
