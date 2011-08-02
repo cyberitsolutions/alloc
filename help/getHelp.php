df810f2a667341269c7969e1e6fd3a16118d2dd5
Alex Lance <code@alexlance.com>
2011-08-02 16:27:06 +1000

    BUG Fixed broken help page that occurred when help boxes were clicked.

--- help/getHelp.php
+++ help/getHelp.php
@@ -24,7 +24,7 @@ require_once("../alloc.php");
 
 if ($_GET["topic"]) {
   $topic = $_GET["topic"];
-  $TPL["str"] = html_entity_decode(stripslashes(page::get_help_string($topic)));
+  $TPL["str"] = @file_get_contents($TPL["url_alloc_help"].$topic.".html");
 
 } else {
   $TPL["str"] = "No valid help topic specified.";
