6880e450e986a86a82c99b7a8e7c0ba47b7e2dfb
Alex Lance <alla@cyber.com.au>
2011-11-11 21:24:12 +1100

    20032 Refactor comment adding code into comment library.

--- comment/lib/comment.inc.php
+++ comment/lib/comment.inc.php
@@ -954,6 +954,168 @@ class comment extends db_entity {
 
 
 
+  // All you need to add a comment, add interested parties, attachments, and re-email it out
+
+  function add_comment($entity,$entityID,$comment_text) {
+    if ($entity && $entityID && $comment_text) {
+      $comment = new comment;
+      $comment->updateSearchIndexLater = true;
+      $comment->set_value('commentType', $entity);
+      $comment->set_value('commentLinkID', $entityID);
+      $comment->set_value('comment', rtrim($comment_text));
+      $comment->save();
+      return $comment->get_id();
+    }
+  }
+
+  function add_interested_parties($commentID,$ip=array(),$op=array()) {
+
+    // We send this email to the default from address, so that a copy of the
+    // original email is kept. The receiveEmail.php script will see that this
+    // email is *from* the same address, and will then skip over it, when going
+    // through the new emails.
+    if (defined("ALLOC_DEFAULT_FROM_ADDRESS") && ALLOC_DEFAULT_FROM_ADDRESS) {
+      list($from_address,$from_name) = parse_email_address(ALLOC_DEFAULT_FROM_ADDRESS);
+      $emailRecipients[] = $from_address;
+    }
+
+    interestedParty::make_interested_parties("comment",$commentID,$ip);
+    $emailRecipients[] = "interested";
+
+    // Other parties that are added on-the-fly
+    foreach ($op as $email => $info) {
+      if ($email && in_str("@",$email)) {
+        unset($lt,$gt); // used above
+        $str = $info["name"];
+        $str and $str.=" ";
+        $str and $lt = "<";
+        $str and $gt = ">";
+        $str.= $lt.str_replace(array("<",">"),"",$email).$gt;
+        $emailRecipients[] = $str;
+
+        // Add a new client contact
+        if ($info["addContact"] && $info["clientID"]) {
+          $q = sprintf("SELECT * FROM clientContact WHERE clientID = %d AND clientContactEmail = '%s'"
+                      ,$info["clientID"],db_esc(trim($email)));
+          $db = new db_alloc();
+          if (!$db->qr($q)) {
+            $cc = new clientContact;
+            $cc->set_value("clientContactName",trim($info["name"]));
+            $cc->set_value("clientContactEmail",trim($email));
+            $cc->set_value("clientID",sprintf("%d",$info["clientID"]));
+            $cc->save();
+          }
+        }
+        // Add the person to the interested parties list
+        if ($info["addIP"] && !interestedParty::exists("comment",$commentID,trim($email))) {
+          $interestedParty = new interestedParty;
+          $interestedParty->set_value("fullName",trim($info["name"]));
+          $interestedParty->set_value("emailAddress",trim($email));
+          $interestedParty->set_value("entityID",$commentID);
+          $interestedParty->set_value("entity","comment");
+          $interestedParty->set_value("external","1");
+          $interestedParty->set_value("interestedPartyActive","1");
+          if (is_object($cc) && $cc->get_id()) {
+            $interestedParty->set_value("clientContactID",$cc->get_id());
+          }
+          $interestedParty->save();
+        }
+      }
+    }
+    return $emailRecipients;
+  }
+
+  function send_comment($commentID, $emailRecipients) {
+
+    $comment = new comment();
+    $comment->set_id($commentID);
+    $comment->select();
+    $comment->from["commentID"] = $comment->get_id();
+    $comment->from["entity"] = "comment";
+    $comment->from["entityID"] = $comment->get_id();
+
+    $token = new token;
+
+    if ($comment->get_value("commentType") == "comment" && $comment->get_value("commentLinkID")) {
+      $c = new comment;
+      $c->set_id($comment->get_value("commentLinkID"));
+      $c->select();
+      if ($token->select_token_by_entity_and_action("comment",$c->get_id(),"add_comment_from_email")) {
+        $comment->from["hash"] = $token->get_value("tokenHash");
+      }
+    }
+
+    if (!$comment->from["hash"]) {
+      if ($token->select_token_by_entity_and_action("comment",$comment->get_id(),"add_comment_from_email")) {
+        $comment->from["hash"] = $token->get_value("tokenHash");
+      } else {
+        $comment->from["hash"] = $comment->make_token_add_comment_from_email();
+      }
+    }
+
+    list($successful_recipients,$messageid) = $comment->send_emails($emailRecipients);
+
+    // Append success to end of the comment
+    if ($successful_recipients && is_object($comment)) {
+      $append_comment_text = "Email sent to: ".$successful_recipients;
+      $message_good.= $append_comment_text;
+      $comment->set_value("commentEmailMessageID",$messageid);
+      $comment->set_value("commentEmailRecipients",$successful_recipients);
+    }
+
+    $comment->skip_modified_fields = true;
+    $comment->save();
+  }
+
+  function attach_timeSheet($commentID, $entityID, $options) {
+    // Begin buffering output to halt anything being sent to the web browser.
+    ob_start();
+    $t = new timeSheetPrint();
+    $ops = query_string_to_array($options);
+
+    $t->get_printable_timeSheet_file($entityID,$ops["timeSheetPrintMode"],$ops["printDesc"],$ops["format"]);
+
+    // Capture the output into $str
+    $str = (string)ob_get_clean();
+
+    $suffix = ".html";
+    $ops["format"] != "html" and $suffix = ".pdf";
+
+    $timeSheetPrintOptions = config::get_config_item("timeSheetPrintOptions");
+    $dir = ATTACHMENTS_DIR."comment".DIRECTORY_SEPARATOR.$commentID;
+    if (!is_dir($dir)) {
+      mkdir($dir, 0777);
+    }
+    $file = $dir.DIRECTORY_SEPARATOR."timeSheet_".$entityID.$suffix;
+    file_put_contents($file,$str);
+  }
+
+  function attach_invoice($commentID,$entityID,$verbose) {
+    // Begin buffering output to halt anything being sent to the web browser.
+    ob_start();
+    $invoice = new invoice();
+    $invoice->set_id($entityID);
+    $invoice->select();
+    $invoice->generate_invoice_file($verbose,true);
+
+    // Capture the output into $str
+    $str = (string)ob_get_clean();
+
+    $suffix = ".pdf";
+
+    $dir = ATTACHMENTS_DIR."comment".DIRECTORY_SEPARATOR.$commentID;
+    if (!is_dir($dir)) {
+      mkdir($dir, 0777);
+    }
+    $file = $dir.DIRECTORY_SEPARATOR."invoice_".$_REQUEST["entityID"].$suffix;
+    file_put_contents($file,$str);
+  }
+
+  function move_attachment($entity, $entityID) {
+    move_attachment($entity, $entityID);
+  }
+
+
 }
 
 
