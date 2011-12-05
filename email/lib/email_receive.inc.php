078b625f0c2c1abb537b4352ba88c28b4204b649
Alex Lance <alla@cyber.com.au>
2011-12-05 22:32:55 +1100

    15275 Using the wrong bitwise comparator for email function masks.

--- email/lib/email_receive.inc.php
+++ email/lib/email_receive.inc.php
@@ -132,7 +132,7 @@ class alloc_email_receive {
   }
 
   function download_email_part($num,$encoding) {
-    $raw_data = imap_fetchbody($this->connection, $this->msg_uid, $num, FT_UID & FT_INTERNAL & FT_PEEK);
+    $raw_data = imap_fetchbody($this->connection, $this->msg_uid, $num, FT_UID | FT_INTERNAL | FT_PEEK);
     return $this->decode_part($encoding,$raw_data);
   }
 
@@ -160,7 +160,7 @@ class alloc_email_receive {
       return $cache[$msg_uid];
     }
     $header = imap_fetchheader($this->connection,$msg_uid, FT_UID);
-    $body = imap_body($this->connection,$msg_uid,FT_UID & FT_INTERNAL);
+    $body = imap_body($this->connection,$msg_uid,FT_UID | FT_INTERNAL);
     $cache[$msg_uid] = array($header,$body);
     return $cache[$msg_uid];
   }
@@ -177,7 +177,7 @@ class alloc_email_receive {
 
     foreach ($this->mail_parts as $v) {
       $s = $v["part_object"]; // structure
-      $raw_data = imap_fetchbody($this->connection, $this->msg_uid, $v["part_number"],FT_UID & FT_INTERNAL & FT_PEEK); 
+      $raw_data = imap_fetchbody($this->connection, $this->msg_uid, $v["part_number"],FT_UID | FT_INTERNAL | FT_PEEK); 
       $thing = $this->decode_part($s->encoding,$raw_data);
 
       if (!$this->mail_text && strtolower($this->mime_types[$s->type]."/".$s->subtype) == "text/plain") {
@@ -202,7 +202,7 @@ class alloc_email_receive {
 
   function mark_seen() {
     imap_setflag_full($this->connection, $this->msg_uid, "\\SEEN", FT_UID); // this doesn't work!
-    $body = imap_body($this->connection,$this->msg_uid,FT_UID & FT_INTERNAL); // this seems to force it to be marked seen
+    $body = imap_body($this->connection,$this->msg_uid,FT_UID | FT_INTERNAL); // this seems to force it to be marked seen
   }
 
   function mark_unseen() {
@@ -211,7 +211,7 @@ class alloc_email_receive {
 
   function forward($address,$subject) {
     $header = imap_fetchheader($this->connection,$this->msg_uid,FT_UID);
-    $body = imap_body($this->connection,$this->msg_uid,FT_UID & FT_INTERNAL);
+    $body = imap_body($this->connection,$this->msg_uid,FT_UID | FT_INTERNAL);
 
     $email = new alloc_email();
     $email->set_headers($header);
