931c0f6d6107027e9382dfca3d3c2d7e9812b8a4
Alex Lance <code@alexlance.com>
2012-01-11 00:28:16 +1100

    20400 Moved error messages out of db_data and into db_triggers.

--- installation/db_data.sql
+++ installation/db_data.sql
@@ -523,13 +523,4 @@ INSERT INTO role (roleID, roleName, roleHandle, roleLevel, roleSequence) VALUES 
 INSERT INTO tokenAction (tokenActionID,tokenAction,tokenActionType,tokenActionMethod) VALUES (2,'Add Comments to Comment','comment','add_comment_from_email');
 
 
--- Error messages for mysql
-INSERT INTO error (errorID) VALUES ("\n\nALLOC ERROR: Not permitted to change time sheet status.\n\n");
-INSERT INTO error (errorID) VALUES ("\n\nALLOC ERROR: Not permitted to delete time sheet unless status is edit.\n\n");
-INSERT INTO error (errorID) VALUES ("\n\nALLOC ERROR: Time sheet is not editable.\n\n");
-INSERT INTO error (errorID) VALUES ("\n\nALLOC ERROR: Task is not editable.\n\n");
-INSERT INTO error (errorID) VALUES ("\n\nALLOC ERROR: Invalid date.\n\n");
-INSERT INTO error (errorID) VALUES ("\n\nALLOC ERROR: Task is not deletable.\n\n");
-
-
 
