ec26bdf1c47570129972e46e9ff71e9cf41912a4
Alex Lance <alla@cyber.com.au>
2012-01-28 12:21:27 +1100

    18525 Make patch file consistent with db structure files.

--- patches/patch-00207-jeremyc.sql
+++ patches/patch-00207-jeremyc.sql
@@ -3,13 +3,13 @@
 INSERT INTO config (name,value,type) VALUES ('defaultTimeSheetRate', '', 'text');
 INSERT INTO config (name,value,type) VALUES ('defaultTimeSheetUnit', 1, 'text');
 
-ALTER TABLE project ADD defaultTimeSheetRate decimal(19,2);
-ALTER TABLE project ADD defaultTimeSheetRateUnitID int(11);
+ALTER TABLE project ADD defaultTimeSheetRate BIGINT DEFAULT NULL;
+ALTER TABLE project ADD defaultTimeSheetRateUnitID integer DEFAULT NULL;
 
 ALTER TABLE project ADD CONSTRAINT project_defaultTimeSheetUnit FOREIGN KEY (defaultTimeSheetRateUnitID) REFERENCES timeUnit (timeUnitID);
 
-ALTER TABLE person ADD defaultTimeSheetRate decimal(19,2);
-ALTER TABLE person ADD defaultTimeSheetRateUnitID int(11);
+ALTER TABLE person ADD defaultTimeSheetRate BIGINT DEFAULT NULL;
+ALTER TABLE person ADD defaultTimeSheetRateUnitID integer DEFAULT NULL;
 
 ALTER TABLE person ADD CONSTRAINT person_defaultTimeSheetUnit FOREIGN KEY (defaultTimeSheetRateUnitID) REFERENCES timeUnit (timeUnitID);
 
