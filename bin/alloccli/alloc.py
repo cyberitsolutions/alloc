206ec661c292754aaecb4c53b1569c9c418b3d5d
Alex Lance <code@alexlance.com>
2012-03-06 14:29:08 +1100

    CLI Refactored table formatting code out of alloc.py.

--- bin/alloccli/alloc.py
+++ bin/alloccli/alloc.py
@@ -375,6 +375,10 @@ class alloc(object):
     a = alloc_cli_arg_handler()
     return a.get_args(self, command_list, ops, s)
 
+  def print_table(self, entity, rows, only_these_fields, sort=False, transforms=None):
+    t = alloc_output_handler()
+    return t.print_table(self, entity, rows, only_these_fields, sort, transforms)
+
   def get_my_personID(self, nick=None):
     """Get current user's personID."""
     ops = {}
@@ -385,225 +389,6 @@ class alloc(object):
     rtn = self.get_list("person", ops)
     for i in rtn:
       return i
-
-  def __get_only_these_fields(self, entity, rows, only_these_fields):
-    """Reduce a list by removing certain columns/fields."""
-    rtn = []
-    inverted_field_names = dict([[v, k] for k, v in self.field_names[entity].items()])
-
-    # Allow the display of custom fields
-    if type(only_these_fields) == type("string"):
-      # Print all fields
-      if only_these_fields.lower() == "all":
-        for k, v in rows.items():
-          for name, value in v.items():
-            del(value) # pylint
-            rtn.append(name)
-            if name in self.field_names[entity]:
-              rtn.append(self.field_names[entity][name])
-            else:
-              rtn.append(name)
-          break
-      # Print a selection of fields
-      else:
-        f = only_these_fields.split(",")
-        for name in f:
-          if name in inverted_field_names:
-            name = inverted_field_names[name]
-          rtn.append(name)
-          if name in self.field_names[entity]:
-            rtn.append(self.field_names[entity][name])
-          else:
-            rtn.append(name)
-      return rtn
-    return only_these_fields
-
-  def __get_sorted_rows(self, entity, rows, sortby):
-    """Sort the rows of a list."""
-    f = '' # satisfy pylint
-    rows = rows.items()
-    if not sortby:
-      return rows
-    inverted_field_names = dict([[v, k] for k, v in self.field_names[entity].items()])
-
-    sortby = sortby.split(",")
-    sortby.reverse()    
-
-    # load up fields
-    k, fields = rows[0]
-
-    # Check that any attempted sortby columns are actually in the table
-    for k in sortby:
-      # Strip leading underscore (used in reverse sorting eg: _Rate)
-      if re.sub("^_", "", k) not in fields and re.sub("^_", "", k) not in inverted_field_names:
-        self.err("Sort column not found: "+k)
-
-    def sort_func(row):
-      """Callback function to return the actual value that should be sorted on."""
-      try:
-        val = row[1][f]
-      except:
-        try:
-          val = row[1][inverted_field_names[f]]
-        except:
-          return ''
-
-      # val is the actual value in the field
-      try:
-        return int(val)
-      except:
-        try:
-          return float(val)
-        except:
-          try:
-            return val.lower()
-          except:
-            return val
-
-    for f in sortby:
-      reverse = False
-      if f and f[0] == "_":
-        reverse = True
-        f = f[1:] # chop leading underscore
-      rows = sorted(rows, key=sort_func, reverse=reverse)
-    return rows
-
-  def __get_widest_field_lengths(self, rows, field_names):
-    """Obtain max lengths of rows of fields."""
-    # Note that an empty field's column header can still have additional
-    # padding, if another value in its column is wider.
-    x = 0
-    lengths = {}
-    for row in rows:
-      for v in row:
-        v = str(v)
-        fn = str(field_names[x])
-        if len(v) < len(fn):
-          v = fn
-        if fn not in lengths or (fn in lengths and len(v) > int(lengths[fn])):
-          lengths[fn] = len(v)
-        x += 1
-      x = 0
-    return lengths
-
-  def __fit_rows_to_screen(self, rows, field_names, width):
-    """Truncate the final column in a table so that it fits on the screen."""
-
-    # We only truncate the rows if we've been configured to
-    if 'alloc_trunc' not in self.config or not self.config['alloc_trunc']:
-      return rows
-
-    lengths = self.__get_widest_field_lengths(rows, field_names)
-    rows2 = []
-    for row in rows:
-      s = ''
-      sep = ''
-      x = 0
-      for v in row:
-        v = str(v)
-        fn = str(field_names[x])
-        if len(v) < len(fn):
-          v = fn
-        if len(v) < lengths[fn]:
-          v = v.ljust(lengths[fn])
-        s += sep+v
-        sep = ' | '
-        x += 1
-
-      # Simulate a normal row in the table
-      s = '| '+s+' |'
-
-      # Useful for debugging
-      # print s
-
-      # fn will be the final cell in the row
-      sum_of_bits = 0
-      for k, l in lengths.items():
-        if k != fn:
-          sum_of_bits += l+3
-
-      # If the row is wider than the width of the terminal
-      if len(s) > width and sum_of_bits+len(fn)+3 < width:
-        end = len(row)-1
-        row[end] = row[end].ljust(lengths[fn])[:-(len(s)-width)]
-      rows2.append(row)
-    return rows2
-
-  def print_table(self, entity, rows, only_these_fields, sort=False, transforms=None):
-    """For printing out results in an ascii table or CSV format."""
-    if self.quiet: return
-    if not rows: return 
-
-    only_these_fields = self.__get_only_these_fields(entity, rows, only_these_fields)
-    field_names = only_these_fields[1::2]
-
-    # Re-order the table, this changes the dict to a list i.e. dict.items().
-    rows = self.__get_sorted_rows(entity, rows, sort)
-    if rows:
-      rows2 = []
-      for k_, row in rows:
-        row = self.__get_row(entity, row, only_these_fields, transforms)
-        rows2.append(row)
-      rows = rows2
- 
-    if self.csv:
-      csv_table = csv.writer(sys.stdout)
-      for row in rows:
-        csv_table.writerow([unicode(s).encode('utf-8') for s in row])
-
-    else:
-      table = PrettyTable()
-      table.set_field_names(field_names)
-      for label in field_names:
-        if '$' in label:
-          table.set_field_align(label, "r")
-        else:
-          table.set_field_align(label, "l")
-
-      if stdout.isatty():
-        proc = subprocess.Popen(['stty', 'size'], stdout=subprocess.PIPE, stderr=open("/dev/null", "w"))
-        ret = proc.wait()
-        if ret == 0:
-          height_, width = proc.communicate()[0].split()
-          width = int(width)
-          rows = self.__fit_rows_to_screen(rows, field_names, width)
-      for row in rows:
-        table.add_row(row)
-      print unicode(table.get_string(header=True)).encode('utf-8')
-
-  def __get_row(self, entity, row, only_these_fields, transforms=None):
-    """Load up the items for one row for a pretty table or csv output."""
-    r = []
-    for v in only_these_fields[::2]: 
-      value = ''
-      success = False
-          
-      if v in row:
-        value = row[v]
-        success = True
-
-      if transforms and v in transforms:
-        value = transforms[v](value)
-        success = True
-
-      if v in self.user_transforms:
-        value = self.user_transforms[v](value, row)
-        success = True
-
-      if v in self.field_names[entity]:
-        other_v = self.field_names[entity][v]
-        if other_v in self.user_transforms:
-          value = self.user_transforms[other_v](value, row)
-          success = True
-
-      if not success:
-        self.err('Bad field name: '+str(v))
-
-      if not value:
-        value = ''
-
-      r.append(value)
-    return r
 
   def is_num(self, obj):
     """Return True is the obj is numeric looking."""
@@ -1099,3 +884,234 @@ class alloc_cli_arg_handler:
       alloc.csv = True
     
     return rtn, " ".join(remainder)
+
+
+class alloc_output_handler:
+  def __get_only_these_fields(self, alloc, entity, rows, only_these_fields):
+    """Reduce a list by removing certain columns/fields."""
+    rtn = []
+    inverted_field_names = dict([[v, k] for k, v in alloc.field_names[entity].items()])
+
+    # Allow the display of custom fields
+    if type(only_these_fields) == type("string"):
+      # Print all fields
+      if only_these_fields.lower() == "all":
+        for k, v in rows.items():
+          for name, value in v.items():
+            del(value) # pylint
+            rtn.append(name)
+            if name in alloc.field_names[entity]:
+              rtn.append(alloc.field_names[entity][name])
+            else:
+              rtn.append(name)
+          break
+      # Print a selection of fields
+      else:
+        f = only_these_fields.split(",")
+        for name in f:
+          if name in inverted_field_names:
+            name = inverted_field_names[name]
+          rtn.append(name)
+          if name in alloc.field_names[entity]:
+            rtn.append(alloc.field_names[entity][name])
+          else:
+            rtn.append(name)
+      return rtn
+    return only_these_fields
+
+  def __get_sorted_rows(self, alloc, entity, rows, sortby):
+    """Sort the rows of a list."""
+    f = '' # satisfy pylint
+    rows = rows.items()
+    if not sortby:
+      return rows
+    inverted_field_names = dict([[v, k] for k, v in alloc.field_names[entity].items()])
+
+    sortby = sortby.split(",")
+    sortby.reverse()    
+
+    # load up fields
+    k, fields = rows[0]
+
+    # Check that any attempted sortby columns are actually in the table
+    for k in sortby:
+      # Strip leading underscore (used in reverse sorting eg: _Rate)
+      if re.sub("^_", "", k) not in fields and re.sub("^_", "", k) not in inverted_field_names:
+        self.err("Sort column not found: "+k)
+
+    def sort_func(row):
+      """Callback function to return the actual value that should be sorted on."""
+      try:
+        val = row[1][f]
+      except:
+        try:
+          val = row[1][inverted_field_names[f]]
+        except:
+          return ''
+
+      # val is the actual value in the field
+      try:
+        return int(val)
+      except:
+        try:
+          return float(val)
+        except:
+          try:
+            return val.lower()
+          except:
+            return val
+
+    for f in sortby:
+      reverse = False
+      if f and f[0] == "_":
+        reverse = True
+        f = f[1:] # chop leading underscore
+      rows = sorted(rows, key=sort_func, reverse=reverse)
+    return rows
+
+  def __get_widest_field_lengths(self, rows, field_names):
+    """Obtain max lengths of rows of fields."""
+    # Note that an empty field's column header can still have additional
+    # padding, if another value in its column is wider.
+    x = 0
+    lengths = {}
+    for row in rows:
+      for v in row:
+        v = str(v)
+        fn = str(field_names[x])
+        if len(v) < len(fn):
+          v = fn
+        if fn not in lengths or (fn in lengths and len(v) > int(lengths[fn])):
+          lengths[fn] = len(v)
+        x += 1
+      x = 0
+    return lengths
+
+  def __fit_rows_to_screen(self, alloc, rows, field_names, width):
+    """Truncate the final column in a table so that it fits on the screen."""
+
+    # We only truncate the rows if we've been configured to
+    if 'alloc_trunc' not in alloc.config or not alloc.config['alloc_trunc']:
+      return rows
+
+    lengths = self.__get_widest_field_lengths(rows, field_names)
+    rows2 = []
+    for row in rows:
+      s = ''
+      sep = ''
+      x = 0
+      for v in row:
+        v = str(v)
+        fn = str(field_names[x])
+        if len(v) < len(fn):
+          v = fn
+        if len(v) < lengths[fn]:
+          v = v.ljust(lengths[fn])
+        s += sep+v
+        sep = ' | '
+        x += 1
+
+      # Simulate a normal row in the table
+      s = '| '+s+' |'
+
+      # Useful for debugging
+      # print s
+
+      # fn will be the final cell in the row
+      sum_of_bits = 0
+      for k, l in lengths.items():
+        if k != fn:
+          sum_of_bits += l+3
+
+      # If the row is wider than the width of the terminal
+      if len(s) > width and sum_of_bits+len(fn)+3 < width:
+        end = len(row)-1
+        row[end] = row[end].ljust(lengths[fn])[:-(len(s)-width)]
+      rows2.append(row)
+    return rows2
+
+  def print_table(self, alloc, entity, rows, only_these_fields, sort=False, transforms=None):
+    """For printing out results in an ascii table or CSV format."""
+    if alloc.quiet: return
+    if not rows: return 
+
+    only_these_fields = self.__get_only_these_fields(alloc, entity, rows, only_these_fields)
+    field_names = only_these_fields[1::2]
+
+    # Re-order the table, this changes the dict to a list i.e. dict.items().
+    rows = self.__get_sorted_rows(alloc, entity, rows, sort)
+    if rows:
+      rows2 = []
+      for k_, row in rows:
+        row = self.__get_row(alloc, entity, row, only_these_fields, transforms)
+        rows2.append(row)
+      rows = rows2
+ 
+    if alloc.csv:
+      csv_table = csv.writer(sys.stdout)
+      for row in rows:
+        csv_table.writerow([unicode(s).encode('utf-8') for s in row])
+
+    else:
+      table = PrettyTable()
+      table.set_field_names(field_names)
+      for label in field_names:
+        if '$' in label:
+          table.set_field_align(label, "r")
+        else:
+          table.set_field_align(label, "l")
+
+      if stdout.isatty():
+        proc = subprocess.Popen(['stty', 'size'], stdout=subprocess.PIPE, stderr=open("/dev/null", "w"))
+        ret = proc.wait()
+        if ret == 0:
+          height_, width = proc.communicate()[0].split()
+          width = int(width)
+          rows = self.__fit_rows_to_screen(alloc, rows, field_names, width)
+      for row in rows:
+        table.add_row(row)
+      print unicode(table.get_string(header=True)).encode('utf-8')
+
+  def __get_row(self, alloc, entity, row, only_these_fields, transforms=None):
+    """Load up the items for one row for a pretty table or csv output."""
+    r = []
+    for v in only_these_fields[::2]: 
+      value = ''
+      success = False
+          
+      if v in row:
+        value = row[v]
+        success = True
+
+      if transforms and v in transforms:
+        value = transforms[v](value)
+        success = True
+
+      if v in alloc.user_transforms:
+        value = alloc.user_transforms[v](value, row)
+        success = True
+
+      if v in alloc.field_names[entity]:
+        other_v = alloc.field_names[entity][v]
+        if other_v in alloc.user_transforms:
+          value = alloc.user_transforms[other_v](value, row)
+          success = True
+
+      if not success:
+        alloc.err('Bad field name: '+str(v))
+
+      if not value:
+        value = ''
+
+      r.append(value)
+    return r
+
+
+
+
+
+
+
+
+
+
