fb92421e358a04319fd5e703356e7a7d1b757597
Alex Lance <alla@cyber.com.au>
2012-03-07 14:51:40 +1100

    Update downloadEmail.php to work with fuzzy searches for emails.

--- email/downloadEmail.php
+++ email/downloadEmail.php
@@ -22,7 +22,15 @@
 
 require_once("../alloc.php");
 
-$lockfile = ATTACHMENTS_DIR."mail.lock.person_".$current_user->get_id();
+function dump_email($uid,$mail) {
+  if ($uid) {
+    exit();
+  }
+}
+
+
+
+//$lockfile = ATTACHMENTS_DIR."mail.lock.person_".$current_user->get_id();
 
 $info["host"] = config::get_config_item("allocEmailHost");
 $info["port"] = config::get_config_item("allocEmailPort");
@@ -34,14 +42,71 @@ if (!$info["host"]) {
   alloc_die("Email mailbox host not defined, assuming email fetch function is inactive.");
 }
 
-$mail = new alloc_email_receive($info,$lockfile);
-$mail->open_mailbox(config::get_config_item("allocEmailFolder"));
+if ($_REQUEST["commentID"]) {
+  $c = new comment();
+  $c->set_id($_REQUEST["commentID"]);
+  $c->select();
 
-if ($_GET["msg_uid"]) {
-  //header('Content-Disposition: attachment; filename="task_comment_email.txt"');
-  header('Content-Type: text/plain; charset=utf-8');
-  list($h,$b) = $mail->get_raw_email_by_msg_uid($_GET["msg_uid"]);
+  $entity = $c->get_value("commentMaster");
+  $entityID = $c->get_value("commentMasterID");
+
+  $mail = new alloc_email_receive($info);
+  $mail->open_mailbox(config::get_config_item("allocEmailFolder").".".$entity.$entityID);
+
+  if ($_REQUEST["uid"]) {
+    header('Content-Type: text/plain; charset=utf-8');
+    list($h,$b) = $mail->get_raw_email_by_msg_uid($_REQUEST["uid"]);
+    $mail->close();
+    echo $h.$b;
+    exit();
+  }
+
+  //$uids = $mail->get_all_email_msg_uids();
+
+  $t = new token();
+  $t->select_token_by_entity_and_action($c->get_value("commentType"),$c->get_value("commentLinkID"),"add_comment_from_email");
+  $hash = $t->get_value("tokenHash");
+
+  // First try a messageID search
+  if ($c->get_value("commentEmailMessageID")) {
+    $str = sprintf('TEXT "%s"',$c->get_value("commentEmailMessageID"));
+    $uids = $mail->get_emails_UIDs_search($str);
+    if (count($uids) == 1) {
+      alloc_redirect($TPL["url_alloc_downloadEmail"]."commentID=".$_REQUEST["commentID"]."&uid=".$uids[0]);
+    } else if (count($uids) > 1) {
+      $all_uids += $uids;
+    }
+  }
+
+  // Next try a hash lookup
+  if ($hash) {
+    $str = sprintf('TEXT "%s"',$hash);
+    $uids = $mail->get_emails_UIDs_search($str);
+    $uids and $all_uids += $uids;
+  }
+
+
+  $str = sprintf('FROM "%s" ',$c->get_value("commentCreatedUserText"));
+  $str.= sprintf(' ON "%s"',format_date("d-M-Y",$c->get_value("commentCreatedTime")));
+  $uids = $mail->get_emails_UIDs_search($str);
+  $uids and $all_uids += $uids;
+
+
+  // Couldn't get a body text search to work! Refuses to match long needles.
+  //echo "<br><br>Using FROM and DATE:".print_r($uids,1);
+  //$text = $c->get_value("comment");
+  //$text = str_replace('\r\n','\n',$text);
+  //$text = str_replace('\n',' ',$text);
+  //$text = str_replace('\r',' ',$text);
+  //$text = str_replace('"','\"',$text);
+  //$text = substr($text,0,25);
+  //$text = trim($text);
+  //echo "<br><br>--".htmlentities($text)."--<br><br>";
+  //$str = sprintf('BODY "%s"',$text);
+  //$uids = $mail->get_emails_UIDs_search($str);
+  //echo "<br><br>Using BODY:".print_r($uids,1);
+
+  $mail->close();
 }
-$mail->close();
-echo $h.$b;
+
 ?>
