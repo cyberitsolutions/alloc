d18d07c2836ace80a3351d627ff6d913ee9d0e50
Alex Lance <code@alexlance.com>
2012-04-22 16:52:47 +1000

    Nudged version to 1.8.4.

--- bin/alloccli/alloc.py
+++ bin/alloccli/alloc.py
@@ -18,7 +18,7 @@ from alloc_cli_arg_handler import alloc_cli_arg_handler
 class alloc(object):
   """Provide a parent class from which the alloc subcommands can extend"""
 
-  client_version = "1.8.3"
+  client_version = "1.8.4"
   url = ''
   username = ''
   quiet = ''
