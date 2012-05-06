e71f8493ffa1d41886ab4a6e81c7201ef0297267
Alex Lance <code@alexlance.com>
2012-05-06 12:21:23 +1000

    20888 Update email address parser.

--- email/lib/init.php
+++ email/lib/init.php
@@ -28,6 +28,7 @@ require_once(dirname(__FILE__)."/email_receive.inc.php");
 require_once(dirname(__FILE__)."/sentEmailLog.inc.php");
 require_once(dirname(__FILE__)."/command.inc.php");
 require_once(dirname(__FILE__)."/mimeDecode.php");
+require_once(dirname(__FILE__)."/RFC822.php");
 
 
 class email_module extends module {
