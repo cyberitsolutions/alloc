393896f80df35ddba9f0f4f84f7fa46e3e4294e4
Alex Lance <alla@cyber.com.au>
2012-06-13 02:22:42 +1000

    20304 Adjust reminderRecipient patch to nuke indexes, so that the patch applies.

--- patches/patch-00250-jeremyc.sql
+++ patches/patch-00250-jeremyc.sql
@@ -1,4 +1,5 @@
 
+-- move reminder recipients into their own table to permit multiple recipients for the one reminder.
 CREATE TABLE reminderRecipient (
 reminderRecipientID integer NOT NULL auto_increment PRIMARY KEY,
 reminderID integer NOT NULL,
@@ -10,6 +11,10 @@ ALTER TABLE reminderRecipient ADD CONSTRAINT reminderRecipient_reminderID FOREIG
 
 INSERT INTO reminderRecipient (reminderID, personID, metaPersonID) SELECT reminderID, personID, metaPerson FROM reminder;
 INSERT INTO permission (tableName, entityID, roleName, sortKey, actions, comment) VALUES ('reminderRecipient' ,0  ,'' ,NULL ,1+2+4+8 ,NULL);
+
+-- need to nuke indexes before dropping columns
+ALTER TABLE reminder DROP FOREIGN KEY reminder_personID;
+ALTER TABLE reminder DROP KEY reminder_personID;
 
 ALTER TABLE reminder DROP personID;
 ALTER TABLE reminder DROP metaPerson;
