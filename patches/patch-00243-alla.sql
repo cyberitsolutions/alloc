b0f16890ac7369c641392e6e9a41a95ea1d91809
Alex Lance <alla@cyber.com.au>
2012-06-15 16:04:53 +1000

    Remove extra database patches that aren't required - which reimport db_triggers.sql.

--- patches/patch-00243-alla.sql
+++
@@ -1,5 +0,0 @@
-
--- This patch will force a re-apply of the triggers patch
--- We need to re-apply the triggers because they have changed
--- for the time sheet item / task actual start date bug.
-DELETE FROM patchLog WHERE patchName = 'patch-00242-alla.php';
