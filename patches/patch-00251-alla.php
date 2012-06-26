49d3da2be12033ff19a2f63345aafcbce67679be
Alex Lance <alla@cyber.com.au>
2012-06-27 04:19:56 +1000

    Parse error in patch 251.

--- patches/patch-00251-alla.php
+++ patches/patch-00251-alla.php
@@ -5,6 +5,7 @@ if (config::for_cyber()) {
   $db = new db_alloc();
   $db->query("INSERT INTO permission (tableName,entityID,roleName,actions,comment) values ('task',0,'','2','permit clients to re-open tasks, CYBER-ONLY.')");
 } else {
+  $db = new db_alloc();
   // Else need to permit client all to read tasks
   $db->query("update permission set roleName = '' where tableName = 'task' and roleName = 'employee' and actions = 1;");
 }
