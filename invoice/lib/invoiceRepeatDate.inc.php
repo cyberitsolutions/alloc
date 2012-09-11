8872f9aed204423037864f043d41685d39d80b84
Alex Lance <alla@cyber.com.au>
2012-09-11 16:11:38 +1000

    v186 Add invoiceRepeatDate class.

---
+++ invoice/lib/invoiceRepeatDate.inc.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * Copyright (C) 2006-2011 Alex Lance, Clancy Malcolm, Cyber IT Solutions
+ * Pty. Ltd.
+ * 
+ * This file is part of the allocPSA application <info@cyber.com.au>.
+ * 
+ * allocPSA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * allocPSA is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with allocPSA. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class invoiceRepeatDate extends db_entity {
+  public $classname = "invoiceRepeatDate";
+  public $data_table = "invoiceRepeatDate";
+  public $key_field = "invoiceRepeatDateID";
+  public $data_fields = array("invoiceRepeatID"
+                             ,"invoiceDate"
+                             );
+}
+
+
+
+?>
