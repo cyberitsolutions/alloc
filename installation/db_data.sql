b5da5b3aeb34544d81a69af7a0837fa3b3a76613
Alex Lance <code@alexlance.com>
2013-02-22 12:29:08 +1100

    22233 Added taskPrioritySpread and taskPriorityScale as config options.

--- installation/db_data.sql
+++ installation/db_data.sql
@@ -501,7 +501,8 @@ INSERT INTO config (name,value,type) VALUES ('rssShowProject', 'on', 'text');
 INSERT INTO config (name,value,type) VALUES ('allocTabs','a:11:{i:0;s:4:"home";i:1;s:6:"client";i:2;s:7:"project";i:3;s:4:"task";i:4;s:4:"time";i:5;s:7:"invoice";i:6;s:4:"sale";i:7;s:6:"person";i:8;s:4:"wiki";i:9;s:5:"inbox";i:10;s:5:"tools";}','array');
 INSERT INTO config (name,value,type) VALUES ('allocURL','','text');
 INSERT INTO config (name,value,type) VALUES ('allocTimezone','','text');
-
+INSERT INTO config (name,value,type) VALUES ("taskPrioritySpread","20","text");
+INSERT INTO config (name,value,type) VALUES ("taskPriorityScale","8","text");
 
 --
 -- Dumping data for table taskType
---
+++ patches/patch-00277-alla.sql
@@ -0,0 +1,4 @@
+
+-- New values for manipulating the shape of the task priority curve
+INSERT INTO config (name,value,type) VALUES ("taskPrioritySpread","20","text");
+INSERT INTO config (name,value,type) VALUES ("taskPriorityScale","8","text");
