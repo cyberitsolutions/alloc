fa89a1612076851791777e415a8e154aa67d12ec
Alex Lance <code@alexlance.com>
2013-03-12 16:49:40 +1100

    v187 Modify patch to work with bad data in reminder table.

--- patches/patch-00267-alla.sql
+++ patches/patch-00267-alla.sql
@@ -1,5 +1,6 @@
 -- new standard creator/date fields for the reminder table
 ALTER TABLE reminder ADD reminderCreatedTime DATETIME NOT NULL AFTER reminderContent; 
 ALTER TABLE reminder ADD reminderCreatedUser INTEGER NOT NULL AFTER reminderCreatedTime;
+UPDATE reminder SET reminderModifiedUser = 1 WHERE reminderModifiedUser IS NULL OR reminderModifiedUser = '' OR reminderModifiedUser = 0;
 UPDATE reminder SET reminderCreatedUser = reminderModifiedUser;
 ALTER TABLE reminder ADD CONSTRAINT reminder_reminderCreatedUser FOREIGN KEY (reminderCreatedUser) REFERENCES person (personID);
