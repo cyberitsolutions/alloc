071cc22dac70d5449a223ef953dcb95c03491714
Alex Lance <code@alexlance.com>
2013-05-11 11:44:53 +1000

    v187 Add patch to explain repeating invoices functionality cronjob.

---
+++ patches/patch-00282-alla.php
@@ -0,0 +1,11 @@
+<?php
+
+// If you want to take advantage of the new repeating invoices functionality,
+// you need to add a new automated repeating job, something that runs once
+// per day, for example this cronjob:
+
+// 40 5 * * * wget -q -O /dev/null URLFORYOURALLOC/invoice/checkRepeat.php
+
+echo "Delete this patch to continue.";
+
+?>
