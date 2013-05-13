3e71e8b1c68f1eeabe88e4306f958c3bb214e190
Alex Lance <code@alexlance.com>
2013-05-13 14:11:07 +1000

    v187 22605 Ensure reminder advanced notices send correctly.

--- reminder/sendReminders.php
+++ reminder/sendReminders.php
@@ -25,29 +25,56 @@ require_once("../alloc.php");
 
 $db = new db_alloc();
 
-$now = date("YmdHis");
-
-  // do reminders
+// do advanced notice emails
 $query = prepare("SELECT *
-            FROM reminder
-           WHERE (reminderTime IS NULL OR reminderTime < %s)
-             AND reminderActive = 1
-                 ",$now);
+                    FROM reminder
+                   WHERE reminderActive = 1
+                     AND reminderAdvNoticeSent = 0
+                     AND NOW() > 
+                         CASE
+                           WHEN reminderAdvNoticeInterval = 'Minute' THEN DATE_SUB(reminderTime, INTERVAL reminderAdvNoticeValue MINUTE)
+                           WHEN reminderAdvNoticeInterval = 'Hour'   THEN DATE_SUB(reminderTime, INTERVAL reminderAdvNoticeValue HOUR)
+                           WHEN reminderAdvNoticeInterval = 'Day'    THEN DATE_SUB(reminderTime, INTERVAL reminderAdvNoticeValue DAY)
+                           WHEN reminderAdvNoticeInterval = 'Week'   THEN DATE_SUB(reminderTime, INTERVAL reminderAdvNoticeValue WEEK)
+                           WHEN reminderAdvNoticeInterval = 'No'     THEN NULL
+                         END
+                 ");
 
 $db->query($query);
 while ($db->next_record()) {
   $reminder = new reminder();
   $reminder->read_db_record($db);
-  
+  //echo "<br>Adv: ".$reminder->get_id();
   $current_user = new person();
   $current_user->load_current_user($db->f('reminderCreatedUser'));
   singleton("current_user",$current_user);
+  if (!$reminder->is_alive()) {
+    $reminder->deactivate();
+  } else {
+    $reminder->mail_advnotice();
+  }
+}
 
+
+// do reminders
+$query = prepare("SELECT *
+                    FROM reminder
+                   WHERE reminderActive = 1
+                     AND (reminderTime IS NULL OR NOW() > reminderTime)
+                 ");
+
+$db->query($query);
+while ($db->next_record()) {
+  $reminder = new reminder();
+  $reminder->read_db_record($db);
+  //echo "<br>Rem: ".$reminder->get_id();
+  $current_user = new person();
+  $current_user->load_current_user($db->f('reminderCreatedUser'));
+  singleton("current_user",$current_user);
   if (!$reminder->is_alive()) {
     $reminder->deactivate();
   } else {
     $reminder->mail_reminder();
-    $reminder->mail_advnotice();
   }
 }
 
