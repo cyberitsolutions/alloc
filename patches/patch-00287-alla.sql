62b6eed9663ab7d042f05b1eb8feeb7d8d943a12
Alex Lance <code@alexlance.com>
2013-08-18 15:07:20 +1000

    Added scaling value to improve quality of logo in PDFs.

---
+++ patches/patch-00287-alla.sql
@@ -0,0 +1,3 @@
+
+-- new scaling config value to scale the logo image in pdfs, this helps tweak the fit and quality (300 is an ok value)
+INSERT INTO config (name,value,type) VALUES ("logoScaleX",0,"text");
