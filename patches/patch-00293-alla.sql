30d19c485fe5342e1a1f689d3cb322f89408a263
Alex Lance <code@alexlance.com>
2014-04-04 21:37:34 +1100

    Add missing permissions.

---
+++ patches/patch-00293-alla.sql
@@ -0,0 +1,10 @@
+INSERT INTO permission (tableName, entityID, roleName, sortKey, actions, comment) values ('currencyType',0  ,'god'      ,NULL ,1+2+4+8    ,NULL);
+INSERT INTO permission (tableName, entityID, roleName, sortKey, actions, comment) values ('itemType',0  ,'god'      ,NULL ,1+2+4+8    ,NULL);
+INSERT INTO permission (tableName, entityID, roleName, sortKey, actions, comment) values ('clientStatus',0  ,'god'      ,NULL ,1+2+4+8    ,NULL);
+INSERT INTO permission (tableName, entityID, roleName, sortKey, actions, comment) values ('projectStatus',0  ,'god'      ,NULL ,1+2+4+8    ,NULL);
+INSERT INTO permission (tableName, entityID, roleName, sortKey, actions, comment) values ('taskStatus',0  ,'god'      ,NULL ,1+2+4+8    ,NULL);
+INSERT INTO permission (tableName, entityID, roleName, sortKey, actions, comment) values ('skillProficiency',0  ,'god'      ,NULL ,1+2+4+8    ,NULL);
+INSERT INTO permission (tableName, entityID, roleName, sortKey, actions, comment) values ('transactionType',0  ,'god'      ,NULL ,1+2+4+8    ,NULL);
+INSERT INTO permission (tableName, entityID, roleName, sortKey, actions, comment) values ('timeSheetItemMultiplier',0  ,'god'      ,NULL ,1+2+4+8    ,NULL);
+INSERT INTO permission (tableName, entityID, roleName, sortKey, actions, comment) values ('taskType',0  ,'god'      ,NULL ,1+2+4+8    ,NULL);
+
