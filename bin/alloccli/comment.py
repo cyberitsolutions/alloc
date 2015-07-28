07b90c2247e475890fbd027ce74bb058f998762f
Christopher Bayliss <christopher.j.bayliss@gmail.com>
2015-07-28 13:22:55 +1000

    Run autopep8 on comment.py

--- bin/alloccli/comment.py
+++ bin/alloccli/comment.py
@@ -1,25 +1,26 @@
-"""alloccli subcommand for adding comments."""
+# alloccli subcommand for adding comments.
 from alloc import alloc
 import sys
 
 
 class comment(alloc):
-  """Add a new comment to a task, project, time sheet or client."""
 
-  # Setup the options that this cli can accept
-  ops = []
-  ops.append((''  , 'help           ', 'Show this help.'))
-  ops.append(('t.', 'task=ID|NAME   ', 'A task ID, or a fuzzy match for a task name.')) 
-  ops.append(('p.', 'project=ID|NAME', 'A project ID, or a fuzzy match for a project name.'))
-  ops.append(('c.', 'client=ID|NAME ', 'A client ID, or a fuzzy match for a client name.')) 
-  ops.append(('i.', 'timesheet=ID   ', 'A time sheet ID.')) 
-  ops.append(('r:', 'to=RECIPIENTS  ', 'Can be usernames, fullnames and/or email, "default",\n'
-                                       '"internal" or "nobody".')) 
+    """Add a new comment to a task, project, time sheet or client."""
 
-  # Specify some header and footer text for the help text
-  help_text = "Usage: %s [OPTIONS]\n"
-  help_text += __doc__
-  help_text += """\n\n%s
+    # Setup the options that this cli can accept
+    ops = []
+    ops.append((''  , 'help           ', 'Show this help.'))
+    ops.append(('t.', 'task=ID|NAME   ', 'A task ID, or a fuzzy match for a task name.'))
+    ops.append(('p.', 'project=ID|NAME', 'A project ID, or a fuzzy match for a project name.'))
+    ops.append(('c.', 'client=ID|NAME ', 'A client ID, or a fuzzy match for a client name.'))
+    ops.append(('i.', 'timesheet=ID   ', 'A time sheet ID.'))
+    ops.append(('r:', 'to=RECIPIENTS  ', 'Can be usernames, fullnames and/or email, "default",\n'
+                                         '"internal" or "nobody".'))
+
+    # Specify some header and footer text for the help text
+    help_text = "Usage: %s [OPTIONS]\n"
+    help_text += __doc__
+    help_text += """\n\n%s
 
 The comment text is read from standard in. If you omit --to, the default
 recipients are assumed. Due to a python bug, you may need to hit ctrl-d twice if
@@ -52,132 +53,141 @@ In this example the recipients are going to be internal only, except for Clyde
   $ alloc comment --task 1234 --to internal --to "Clyde Client <cc@example.com>"
 """
 
-  def run(self, command_list):
-    """Execute subcommand."""
+    # Execute subcommand.
+    def run(self, command_list):
 
-    # Get the command line arguments into a dictionary
-    o, remainder_ = self.get_args(command_list, self.ops, self.help_text)
+        # Get the command line arguments into a dictionary
+        o, remainder_ = self.get_args(command_list, self.ops, self.help_text)
 
-    # Got this far, then authenticate
-    self.authenticate()
+        # Got this far, then authenticate
+        self.authenticate()
 
-    self.msg('Enter comment (ctrl-c to cancel, ctrl-d to send)')
+        self.msg('Enter comment (ctrl-c to cancel, ctrl-d to send)')
 
-    personID = self.get_my_personID()
-    taskID = 0
-    projectID = 0
-    timeSheetID = 0
-    clientID = 0
-    entity = ''
-    entityID = 0
-    recipients = {}
+        personID = self.get_my_personID()
+        taskID = 0
+        projectID = 0
+        timeSheetID = 0
+        clientID = 0
+        entity = ''
+        entityID = 0
+        recipients = {}
 
-    # Get a taskID either passed via command line, or figured out from a task name
-    if self.is_num(o['task']):
-      taskID = o['task']
-    elif o['task']:
-      taskID = self.search_for_task({ 'taskName': o['task'], 'taskView': 'prioritised' })
+        # Get a taskID either passed via command line, or figured out from a
+        # task name
+        if self.is_num(o['task']):
+            taskID = o['task']
+        elif o['task']:
+            taskID = self.search_for_task(
+                {'taskName': o['task'], 'taskView': 'prioritised'})
 
-    # Get a projectID either passed via command line, or figured out from a project name
-    if self.is_num(o['project']):
-      projectID = o['project']
-    elif o['project']:
-      projectID = self.search_for_project(o['project'], personID)
+        # Get a projectID either passed via command line, or figured out from a
+        # project name
+        if self.is_num(o['project']):
+            projectID = o['project']
+        elif o['project']:
+            projectID = self.search_for_project(o['project'], personID)
 
-    # Get a clientID either passed via command line, or figured out from a client name
-    if self.is_num(o['client']):
-      clientID = o['client']
-    elif o['client']:
-      clientID = self.search_for_client(o['client'])
+        # Get a clientID either passed via command line, or figured out from a
+        # client name
+        if self.is_num(o['client']):
+            clientID = o['client']
+        elif o['client']:
+            clientID = self.search_for_client(o['client'])
 
-    # Get a timesheetID passed via command line
-    if self.is_num(o['timesheet']):
-      timeSheetID = o['timesheet']
+        # Get a timesheetID passed via command line
+        if self.is_num(o['timesheet']):
+            timeSheetID = o['timesheet']
 
-    # Print out entity
-    try:
+        # Print out entity
+        try:
 
-      if taskID:
-        entity = 'task'
-        entityID = taskID
-        k_, v = self.get_list(entity, { entity+'ID' : entityID, 'taskView' : 'prioritised' }).popitem()
-        self.msg(v['taskTypeID']+': '+v['taskID']+' '+v['taskName'])
-      elif projectID:
-        entity = 'project'
-        entityID = projectID
-        k_, v = self.get_list(entity, { entity+'ID' : entityID }).popitem()
-        self.msg(v['projectType']+': '+v['projectID']+' '+v['projectName'])
-      elif timeSheetID:
-        entity = 'timeSheet'
-        entityID = timeSheetID
-        k_, v = self.get_list(entity, { entity+'ID' : entityID }).popitem()
-        self.msg('Time Sheet: '+v['timeSheetID']+' '+v['projectName'] +' '+ v['amount'])
-      elif clientID:
-        entity = 'client'
-        entityID = clientID
-        k_, v = self.get_list(entity, { entity+'ID' : entityID }).popitem()
-        self.msg('Client: '+v['clientID']+' '+v['clientName'])
+            if taskID:
+                entity = 'task'
+                entityID = taskID
+                k_, v = self.get_list(
+                    entity, {entity + 'ID': entityID, 'taskView': 'prioritised'}).popitem()
+                self.msg(
+                    v['taskTypeID'] + ': ' + v['taskID'] + ' ' + v['taskName'])
+            elif projectID:
+                entity = 'project'
+                entityID = projectID
+                k_, v = self.get_list(
+                    entity, {entity + 'ID': entityID}).popitem()
+                self.msg(
+                    v['projectType'] + ': ' + v['projectID'] + ' ' + v['projectName'])
+            elif timeSheetID:
+                entity = 'timeSheet'
+                entityID = timeSheetID
+                k_, v = self.get_list(
+                    entity, {entity + 'ID': entityID}).popitem()
+                self.msg(
+                    'Time Sheet: ' + v['timeSheetID'] + ' ' + v['projectName'] + ' ' + v['amount'])
+            elif clientID:
+                entity = 'client'
+                entityID = clientID
+                k_, v = self.get_list(
+                    entity, {entity + 'ID': entityID}).popitem()
+                self.msg('Client: ' + v['clientID'] + ' ' + v['clientName'])
 
-    except:
-      self.die("No "+entity+" found with ID "+entityID)
+        except:
+            self.die("No " + entity + " found with ID " + entityID)
 
+        # Assume 'default' if there's no --to
+        if not o['to']:
+            o['to'] = ['default']
 
-    # Assume 'default' if there's no --to
-    if not o['to']:
-      o['to'] = ['default']
+        # Sort out recipients
+        nobody = False
+        comma = ""
+        for p in o['to']:
+            if p.lower() == 'nobody':
+                nobody = True
+                o['to'].remove('nobody')
 
-    # Sort out recipients
-    nobody = False
-    comma = ""
-    for p in o['to']:
-      if p.lower() == 'nobody':
-        nobody = True
-        o['to'].remove('nobody')
+        # We only append current user if they haven't specified 'nobody'
+        if not nobody:
+            o['to'].append(self.username)
 
-    # We only append current user if they haven't specified 'nobody'
-    if not nobody:
-      o['to'].append(self.username)
+        # Magic
+        if entity and entityID:
+            recipients = self.get_people(o['to'], entity, entityID)
 
-    # Magic
-    if entity and entityID:
-      recipients = self.get_people(o['to'], entity, entityID)
+        # Print recipients
+        comma = ""
+        text = ""
+        if recipients:
+            for k_, info in recipients.items():
+                text += comma + (info.get('name') or '') + \
+                    " <" + (info.get('emailAddress') or '') + ">"
+                comma = ", "
+        self.msg("To: " + text)
 
-    # Print recipients
-    comma = ""
-    text = ""
-    if recipients:
-      for k_, info in recipients.items():
-        text += comma + (info.get('name') or '')+ " <"+(info.get('emailAddress') or '')+">"
-        comma = ", "
-    self.msg("To: " + text)
-  
-    if not entity and not entityID:
-      self.die("Exiting. No entity specified (eg --task ID).")
+        if not entity and not entityID:
+            self.die("Exiting. No entity specified (eg --task ID).")
 
-    # Grab stdin
-    try:
-      comment_text = sys.stdin.read()
-    except (IOError):
-      self.die("Exiting. No message specified.")
-    except (KeyboardInterrupt):
-      self.die("Exiting. No message specified.")
+        # Grab stdin
+        try:
+            comment_text = sys.stdin.read()
+        except (IOError):
+            self.die("Exiting. No message specified.")
+        except (KeyboardInterrupt):
+            self.die("Exiting. No message specified.")
 
+        # Send message
+        if comment_text and entity and entityID:
+            package = {}
+            package['ip'] = recipients
+            package['comment_text'] = comment_text
+            package['entity'] = entity
+            package['entityID'] = entityID
+            package['command'] = 'add_comment'
 
-    # Send message
-    if comment_text and entity and entityID:
-      package = {}
-      package['ip'] = recipients
-      package['comment_text'] = comment_text
-      package['entity'] = entity
-      package['entityID'] = entityID
-      package['command'] = 'add_comment'
-
-      args = {}
-      args['entity'] = 'comment'
-      args['id'] = 'new'
-      args['options'] = package
-      args['method'] = 'edit_entity'
-      self.make_request(args)
-    else:
-      self.die("Exiting. No message specified.")
-
+            args = {}
+            args['entity'] = 'comment'
+            args['id'] = 'new'
+            args['options'] = package
+            args['method'] = 'edit_entity'
+            self.make_request(args)
+        else:
+            self.die("Exiting. No message specified.")
