81098ed85f3609c0a04aafa845f2508e76d1576d
Christopher Bayliss <christopher.j.bayliss@gmail.com>
2015-07-28 14:59:01 +1000

    Run autopep8 on view.py

--- bin/alloccli/view.py
+++ bin/alloccli/view.py
@@ -1,22 +1,21 @@
-"""alloccli subcommand for viewing the details of a single entity."""
+# alloccli subcommand for viewing the details of a single entity.
 from alloc import alloc
 
+
 class view(alloc):
-  """View an entity."""
 
-  # Setup the options that this cli can accept
-  ops = []
-  ops.append((''  , 'help           ', 'Show this help.'))
-  ops.append(('t:', 'task=ID        ', 'An existing task\'s ID.'))
-  #ops.append(('i:', 'item=ID        ', 'An existing time sheet item\'s ID.'))
-  #ops.append(('p:', 'project=ID     ', 'An existing project\'s ID.'))
-  #ops.append(('s:', 'timesheet=ID   ', 'An existing time sheet\'s ID.'))
-  ops.append(('c',   'children'       , 'Show children for tasks.'))
+    """View an entity."""
 
-  # Specify some header and footer text for the help text
-  help_text = "Usage: %s [OPTIONS]\n"
-  help_text += __doc__
-  help_text += """\n\n%s
+    # Setup the options that this cli can accept
+    ops = []
+    ops.append((''  , 'help           ', 'Show this help.'))
+    ops.append(('t:', 'task=ID        ', 'An existing task\'s ID.'))
+    ops.append(('c' , 'children       ', 'Show children for tasks.'))
+
+    # Specify some header and footer text for the help text
+    help_text = "Usage: %s [OPTIONS]\n"
+    help_text += __doc__
+    help_text += """\n\n%s
 
 This program allows you to view a single alloc entity, like a task.
 
@@ -25,24 +24,23 @@ Examples:
 # Display all the different fields on a task
 alloc view --task 1234"""
 
-  def run(self, command_list):
-    """Execute subcommand."""
+    def run(self, command_list):
+        # Execute subcommand.
 
-    # Get the command line arguments into a dictionary
-    o, remainder_ = self.get_args(command_list, self.ops, self.help_text)
+        # Get the command line arguments into a dictionary
+        o, remainder_ = self.get_args(command_list, self.ops, self.help_text)
 
-    # Got this far, then authenticate
-    self.authenticate()
+        # Got this far, then authenticate
+        self.authenticate()
 
-    if o['task']:
-      print self.print_task(o['task'], children = o['children'])
+        if o['task']:
+            print self.print_task(o['task'], children=o['children'])
 
-    # elif o['timesheet']:
-    #   pass
+        # elif o['timesheet']:
+        #   pass
 
-    # elif o['item']:
-    #   pass
+        # elif o['item']:
+        #   pass
 
-    # elif o['project']:
-    #   pass
-  
+        # elif o['project']:
+        #   pass
