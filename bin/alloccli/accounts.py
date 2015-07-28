3b56c8bc77cea9cb69759df2c8f7365c823823db
Christopher Bayliss <christopher.j.bayliss@gmail.com>
2015-07-28 10:53:14 +1000

    Run autopep8 on accounts.py, del some unused code

--- bin/alloccli/accounts.py
+++ bin/alloccli/accounts.py
@@ -1,73 +1,68 @@
-"""alloccli subcommand for TF accounts."""
+# alloccli subcommand for TF accounts.
 from alloc import alloc
 
+
 class accounts(alloc):
-  """Print a list of your TF accounts."""
 
-  # Setup the options that this cli can accept
-  ops = []
-  ops.append((''  , 'help           ', 'Show this help.'))
-  ops.append((''  , 'csv=[WHEN]     ', 'Return the results in CSV format. WHEN can be "auto",\n'
-                                       '"never" or "always". If WHEN is omitted, assume "always".'))
-  ops.append(('i' , 'items          ', 'Show accounts\' transactions.'))
-  ops.append(('a:', 'account=TF     ', 'Show a particular TF. Default to your TFs.'))
-  #ops.append(('p:', 'project=ID|NAME', 'A project ID, or a fuzzy match for a project name.'))
-  #ops.append(('s:', 'status=STATUS  ', 'The transactions\' status. Can accept multiple values.\n'
-                                        #'(eg: "pending,approved,rejected" Default: approved)'))
-  #ops.append(('t:', 'time=ID        ', 'A time sheet ID.'))
-  #ops.append(('d:', 'date=YYYY-MM-DD', 'The from date of the earliest transaction.'))
-  #ops.append(('o:', 'order=NAME     ', 'The order the accounts or transactions are displayed in.\n'
-                                        #'Default for accounts: "???" Default for transactions: "???"'))
-  ops.append(('f:', 'field=NAME     ', 'A field you would like printed.\n'
-                                       '(eg: -f all eg: -f tfID -f tfName -f tfBalance)')) 
-  ops.append((''  , 'possible-fields', 'List of possible fields.'))
+    """Print a list of your TF accounts."""
 
-  # Specify some header and footer text for the help text
-  help_text = "Usage: %s [OPTIONS]\n"
-  help_text += __doc__
-  help_text += '''\n\n%s'''
+    # Setup the options that this cli can accept
+    ops = []
+    ops.append((''  , 'help           ', 'Show this help.'))
+    ops.append((''  , 'csv=[WHEN]     ', 'Return the results in CSV format. WHEN can be "auto",\n'
+                                         '"never" or "always". If WHEN is omitted, assume "always".'))
+    ops.append(('i' , 'items          ', 'Show accounts\' transactions.'))
+    ops.append(('a:', 'account=TF     ', 'Show a particular TF. Default to your TFs.'))
+    ops.append(('f:', 'field=NAME     ', 'A field you would like printed.\n'
+                                         '(eg: -f all eg: -f tfID -f tfName -f tfBalance)'))
+    ops.append((''  , 'possible-fields', 'List of possible fields.'))
 
-  def run(self, command_list):
-    """Execute subcommand."""
+    # Specify some header and footer text for the help text
+    help_text = "Usage: %s [OPTIONS]\n"
+    help_text += __doc__
+    help_text += '''\n\n%s'''
 
-    # Get the command line arguments into a dictionary
-    o, remainder_ = self.get_args(command_list, self.ops, self.help_text)
+    def run(self, command_list):
+        """Execute subcommand."""
 
-    # Got this far, then authenticate
-    self.authenticate()
+        # Get the command line arguments into a dictionary
+        o, remainder_ = self.get_args(command_list, self.ops, self.help_text)
 
-    if o['possible-fields']:
-        alloc().possible_fields("transaction")
+        # Got this far, then authenticate
+        self.authenticate()
 
-    # Initialize some variables
-    #self.quiet = o['quiet']
-    ops = {}
-    
-    if 'account' in o and o['account']:
-      ops['tfIDs'] = self.make_request({'method':'get_tfID', 'options':o['account']})
+        if o['possible-fields']:
+            alloc().possible_fields("transaction")
 
-    # Get transactions
-    if 'items' in o and o['items']:
-      if o['field']:
-        fields = o['field']
-      else:
-        fields = ["transactionID", "fromTfName", "tfName", "amount", "status", "transactionDate"]
+        # Initialize some variables
+        #self.quiet = o['quiet']
+        ops = {}
 
-      transactions = self.get_list("transaction", ops)
-      if transactions:
-        self.print_table("transaction", transactions, fields, "transactionDate")
-        print "num rows:", len(transactions)
- 
-    # Get tf
-    else:
-      if o['field']:
-        fields = o['field']
-      else:
-        fields = ["tfID", "tfName", "tfBalancePending", "tfBalance"]
+        if 'account' in o and o['account']:
+            ops['tfIDs'] = self.make_request(
+                {'method': 'get_tfID', 'options': o['account']})
 
-      tfs = self.get_list("tf", ops)
-      if tfs:
-        self.print_table("tf", tfs, fields, "tfName")
+        # Get transactions
+        if 'items' in o and o['items']:
+            if o['field']:
+                fields = o['field']
+            else:
+                fields = ["transactionID", "fromTfName", "tfName",
+                          "amount", "status", "transactionDate"]
 
+            transactions = self.get_list("transaction", ops)
+            if transactions:
+                self.print_table(
+                    "transaction", transactions, fields, "transactionDate")
+                print "num rows:", len(transactions)
 
-   
+        # Get tf
+        else:
+            if o['field']:
+                fields = o['field']
+            else:
+                fields = ["tfID", "tfName", "tfBalancePending", "tfBalance"]
+
+            tfs = self.get_list("tf", ops)
+            if tfs:
+                self.print_table("tf", tfs, fields, "tfName")
