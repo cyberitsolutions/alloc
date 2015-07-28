33bf03220248af1b6937f7b4e3f4cd96db771045
Christopher Bayliss <christopher.j.bayliss@gmail.com>
2015-07-28 14:14:03 +1000

    Run autopep8 on ktokens.py

--- bin/alloccli/ktokens.py
+++ bin/alloccli/ktokens.py
@@ -1,50 +1,50 @@
-"""alloccli subcommand for viewing a list of tokens."""
+# alloccli subcommand for viewing a list of tokens.
 from alloc import alloc
 
+
 class ktokens(alloc):
-  """Retrieve token information."""
 
-  # Setup the options that this cli can accept
-  ops = []
-  ops.append((''  , 'help           ', 'Show this help.'))
-  ops.append((''  , 'csv=[WHEN]     ', 'Return the results in CSV format. WHEN can be "auto",\n'
-                                       '"never" or "always". If WHEN is omitted, assume "always".'))
-  ops.append(('q' , 'quiet          ', 'Run with no output except errors.'))
-  ops.append(('k:', 'key=KEY        ', 'An 8 character email subject line key.'))
-  ops.append(('i:', 'id=ID          ', 'A comment id.'))
+    """Retrieve token information."""
 
-  # Specify some header and footer text for the help text
-  help_text = "Usage: %s [OPTIONS] [FILE]\n"
-  help_text += __doc__
-  help_text += """\n\n%s
+    # Setup the options that this cli can accept
+    ops = []
+    ops.append((''  , 'help           ', 'Show this help.'))
+    ops.append((''  , 'csv=[WHEN]     ', 'Return the results in CSV format. WHEN can be "auto",\n'
+                                         '"never" or "always". If WHEN is omitted, assume "always".'))
+    ops.append(('q' , 'quiet          ', 'Run with no output except errors.'))
+    ops.append(('k:', 'key=KEY        ', 'An 8 character email subject line key.'))
+    ops.append(('i:', 'id=ID          ', 'A comment id.'))
+
+    # Specify some header and footer text for the help text
+    help_text = "Usage: %s [OPTIONS] [FILE]\n"
+    help_text += __doc__
+    help_text += """\n\n%s
 
 Examples:
 
 # Print out a list of tokens
 alloc token --key 1234abcd"""
 
-  def run(self, command_list):
-    """Execute subcommand."""
+    # Execute subcommand.
+    def run(self, command_list):
 
-    # Get the command line arguments into a dictionary
-    o, remainder_ = self.get_args(command_list, self.ops, self.help_text)
+        # Get the command line arguments into a dictionary
+        o, remainder_ = self.get_args(command_list, self.ops, self.help_text)
 
-    # Got this far, then authenticate
-    self.authenticate()
+        # Got this far, then authenticate
+        self.authenticate()
 
-    # Initialize some variables
-    self.quiet = o['quiet']
+        # Initialize some variables
+        self.quiet = o['quiet']
 
-    # This is the data format that is exported and imported
-    fields = ["tokenEntity", "tokenEntityID", "tokenHash"]
-    searchops = {}
-    searchops['tokenHash'] = o['key'] or ''
-    searchops['tokenEntity'] = 'comment'
-    searchops['tokenEntityID'] = o['id'] or ''
+        # This is the data format that is exported and imported
+        fields = ["tokenEntity", "tokenEntityID", "tokenHash"]
+        searchops = {}
+        searchops['tokenHash'] = o['key'] or ''
+        searchops['tokenEntity'] = 'comment'
+        searchops['tokenEntityID'] = o['id'] or ''
 
-    if o['key'] or o['id']:
-      tokens = self.get_list("token", searchops)
-      if tokens:
-        self.print_table("token", tokens, fields)
-
-
+        if o['key'] or o['id']:
+            tokens = self.get_list("token", searchops)
+            if tokens:
+                self.print_table("token", tokens, fields)
