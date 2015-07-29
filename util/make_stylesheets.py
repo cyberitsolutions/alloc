e0c07313713bb72c0137b9056c601dd71a6691ec
Christopher Bayliss <christopher.j.bayliss@gmail.com>
2015-07-29 11:15:38 +1000

    Run autopep8 on agpl_header.py & make_stylesheets.py

--- util/make_stylesheets.py
+++ util/make_stylesheets.py
@@ -1,60 +1,60 @@
 #!/usr/bin/env python
 """Compiled the stylesheets in css/src/"""
 
-import ConfigParser, os, sys
+import ConfigParser
+import os
+import sys
 
 
 def main():
-  """Rebuild CSS"""
+    """Rebuild CSS"""
 
-  # Read the style.ini file with all the config key/values
-  config = ConfigParser.ConfigParser()
+    # Read the style.ini file with all the config key/values
+    config = ConfigParser.ConfigParser()
 
-  # Current directory trickyness
-  d = os.path.dirname(sys.argv[0])+'/'
+    # Current directory trickyness
+    d = os.path.dirname(sys.argv[0]) + '/'
 
+    # Convert the style template into a string
+    fd = open(d + '../css/src/style.tpl')
+    str_orig = fd.read()
+    fd.close()
 
-  # Convert the style template into a string
-  fd = open(d+'../css/src/style.tpl')
-  str_orig = fd.read()
-  fd.close()
+    # List of style_something.ini files in ../styles/
+    files = os.listdir(d + '../css/src/')
 
-  # List of style_something.ini files in ../styles/ 
-  files = os.listdir(d+'../css/src/')
+    # Loop through style_*.ini files
+    for item in files:
 
-  # Loop through style_*.ini files
-  for item in files:
+        if item.endswith('.ini'):
 
-    if item.endswith('.ini'):
+            # Parse each style_something.ini file
+            config.read([d + '../css/src/' + item])
 
-      # Parse each style_something.ini file
-      config.read([d+'../css/src/'+item])
+            # Get a list of sections from the config file
+            sections = config.sections()
 
-      # Get a list of sections from the config file
-      sections = config.sections()
+            # Loop through each section
+            for section in sections:
+                dictops = {}
+                s = str_orig
 
+                # Get all "keys:" from the ini file
+                options = config.options(section)
 
-      # Loop through each section
-      for section in sections:
-        dictops = {}
-        s = str_orig
+                # Load up a dict
+                for option in options:
+                    dictops[option] = config.get(section, option)
 
+                # Do a search and replace for each key/value in the template
+                # string
+                for k, v in dictops.items():
+                    s = s.replace('(' + k.upper() + ')', v)
 
-        # Get all "keys:" from the ini file
-        options = config.options(section)
-
-        # Load up a dict 
-        for option in options:
-          dictops[option] = config.get(section, option)
-        
-        # Do a search and replace for each key/value in the template string 
-        for k, v in dictops.items():
-          s = s.replace('('+k.upper()+')', v)
-
-        # Write it out to a file
-        fd = open(d+'../css/'+section+'.css','w')
-        fd.write(s) 
-        fd.close()
+                # Write it out to a file
+                fd = open(d + '../css/' + section + '.css', 'w')
+                fd.write(s)
+                fd.close()
 
 
 main()
