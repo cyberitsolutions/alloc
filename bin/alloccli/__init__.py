cd66a9033af2800b79d157a8258873700133e134
Christopher Bayliss <christopher.j.bayliss@gmail.com>
2016-05-07 13:53:12 +1000

    Remove bin/
    alloc-cli has be seperated out to a new repo:
    https://github.com/cyberitsolutions/alloc-cli

--- bin/alloccli/__init__.py
+++
@@ -1,5 +0,0 @@
-"""The alloccli modules."""
-__all__ = ["alloc", "work", "comment", "edit", "view", "projects",
-           "tasks", "timesheets", "reminders", "invoices", "accounts",
-           "browse", "mbox", "submit", "subscriptions", "ktokens",
-           "task", "item", "reminder", "version"]
